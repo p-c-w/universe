7c1634c48306e56244f58b325881cf1b1f842f4d
Inae Choi <inaechoi89@gmail.com>
2023-04-21 15:36:09 +0900

    Feat: MyPage Title 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import { Title } from '@mantine/core';
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 77.5rem;
+`;
 
 const MyPage = () => {
   console.log();
-  return <>MyPage</>;
+  return (
+    <Container>
+      <Title>000's Universe</Title>
+    </Container>
+  );
 };
 
 export default MyPage;
