952ea1cee96b77883ce5695b4369ec46d24b7836
Inae Choi <inaechoi89@gmail.com>
2023-04-21 15:52:31 +0900

    Feat: Mypage TopSection 마크업

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -6,11 +6,37 @@ const Container = styled.div`
   width: 77.5rem;
 `;
 
+const TopSection = styled.div`
+  display: flex;
+  gap: 1rem;
+  div {
+    width: 38.75rem;
+  }
+`;
+
 const MyPage = () => {
   console.log();
   return (
     <Container>
       <Title>000's Universe</Title>
+      <TopSection>
+        <div className="subscribe">
+          <Title>예상 구독료</Title>
+          <div className="badges"></div>
+          <div>
+            <div>
+              <h4>현재 나의 구독료</h4>
+              <span>₩29,800</span>
+              <button></button>
+            </div>
+            <div>
+              <h4>구독하고 있지만 보고 있지 않아요</h4>
+              <div className="no-badges"></div>
+            </div>
+          </div>
+        </div>
+        <div className="static">static graph</div>
+      </TopSection>
     </Container>
   );
 };
