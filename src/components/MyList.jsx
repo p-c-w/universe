c21d316819f1e4a72f30b171849e366abe74a069
Inae Choi <inaechoi89@gmail.com>
2023-04-21 15:35:31 +0900

    Feat: MyPage Components layout 생성

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,5 +1,55 @@
+import styled from '@emotion/styled';
 import { Group, Text, Accordion, Button } from '@mantine/core';
 import { Badge } from './index';
+
+// mockdata
+const users = [
+  {
+    email: 'snowlover@gmail.com',
+    password: 'snow123',
+    name: 'snowlover',
+    subscribe_list: [{ id: 8, price: 7900 }],
+    like_list: [
+      { id: 507129, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
+      { id: 37692, type: 'tv', modified_at: '2020-12-31T12:59:32.746Z' },
+    ],
+    watch_list: [{ id: 18465, type: 'movie', modified_at: '2021-05-18T12:59:32.746Z' }],
+    history_list: [
+      { id: 829, type: 'movie', modified_at: '2023-04-15T12:59:32.746Z' },
+      { id: 54823, type: 'tv', modified_at: '2022-01-01T12:59:32.746Z' },
+    ],
+  },
+  {
+    email: 'squid@gmail.com',
+    password: 'squid456',
+    name: 'squid',
+    subscribe_list: [{ id: 8, price: 7900 }],
+    like_list: [
+      { id: 507129, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
+      { id: 37692, type: 'tv', modified_at: '2020-12-31T12:59:32.746Z' },
+    ],
+    watch_list: [{ id: 18465, type: 'movie', modified_at: '2021-05-18T12:59:32.746Z' }],
+    history_list: [
+      { id: 829, type: 'movie', modified_at: '2023-04-15T12:59:32.746Z' },
+      { id: 54823, type: 'tv', modified_at: '2022-01-01T12:59:32.746Z' },
+    ],
+  },
+  {
+    email: 'noname@gmail.com',
+    password: 'noname123',
+    name: 'noname',
+    subscribe_list: [{ id: 8, price: 7900 }],
+    like_list: [
+      { id: 507129, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
+      { id: 37692, type: 'tv', modified_at: '2020-12-31T12:59:32.746Z' },
+    ],
+    watch_list: [{ id: 18465, type: 'movie', modified_at: '2021-05-18T12:59:32.746Z' }],
+    history_list: [
+      { id: 829, type: 'movie', modified_at: '2023-04-15T12:59:32.746Z' },
+      { id: 54823, type: 'tv', modified_at: '2022-01-01T12:59:32.746Z' },
+    ],
+  },
+];
 
 const mockData = [
   {
