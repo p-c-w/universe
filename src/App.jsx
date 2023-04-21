bbe0de69d469d2791fe4529b003e1cf0cbcf5f3a
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-21 21:39:37 +0900

    Feat: queryClient 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { RecoilRoot } from 'recoil';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import useTheme from './hooks/useTheme';
 
 import { Root, SignIn, SignUp } from './pages/index';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -26,15 +30,18 @@ const App = () => {
 
   return (
     <RecoilRoot>
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider
-          theme={{ fontFamily: 'Spoqa Han Sans Neo, sans-serif', colorScheme }}
-          withCSSVariables
-          withGlobalStyles
-          withNormalizeCSS>
-          <RouterProvider router={router} />
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider
+            theme={{ fontFamily: 'Spoqa Han Sans Neo, sans-serif', colorScheme }}
+            withCSSVariables
+            withGlobalStyles
+            withNormalizeCSS>
+            <RouterProvider router={router} />
+          </MantineProvider>
+        </ColorSchemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 };
