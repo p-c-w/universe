5108fdfb22700f2e2fb5945a45dc9eb764d7edec
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-24 10:28:32 +0900

    Feat: SignUp 페이지 구현

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Flex, Container } from '@mantine/core';
+
+import { SignupHeader, SignupForm, SignupOption } from '../components';
+
+const SignUp = () => {
+  const [active, setActive] = useState(1);
+
+  return (
+    <>
+      <SignupHeader />
+      <Flex mih={500} justify="center" direction="column" wrap="wrap">
+        <Container style={{ width: '600px', padding: '20px', border: '1px solid #373a40', borderRadius: '6px' }}>
+          {active === 1 ? <SignupForm setActive={setActive} /> : <SignupOption />}
+        </Container>
+      </Flex>
+    </>
+  );
+};
+
+export default SignUp;
