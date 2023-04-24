f3f247e5e18181bb7527ba0de58d87700d15d29b
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-24 19:59:18 +0900

    Fix: confirmPassword 작동 오류로 코드 수정

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,23 +1,24 @@
-import { z } from 'zod';
-
-// const useridRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-const passwordRegExp = /^[A-Za-z0-9]{6,12}$/;
+import z from 'zod';
 
 const signInSchema = z.object({
-  // email: z.string().regex(useridRegExp, '이메일 형식에 맞게 입력해 주세요.'),
   email: z.string().min(1, '이메일을 입력해 주세요').email('이메일 형식에 맞게 입력해 주세요'),
-  password: z.string().regex(passwordRegExp, '영문 또는 숫자를 6~12자 입력하세요.'),
+  password: z
+    .string()
+    .min(1, '패스워드를 입력해 주세요')
+    .regex(/^[a-zA-Z0-9]+$/, '영문 또는 숫자를 입력해 주세요')
+    .min(6, '6자 이상 입력해 주세요')
+    .max(12, '12자 이하로 입력해 주세요'),
 });
 
-const signUpSchema = signInSchema.and(
-  z
-    .object({
-      confirmPassword: z.string(),
+const signUpSchema = signInSchema
+  .and(
+    z.object({
+      confirmPassword: z.string().min(1, '패스워드를 확인해 주세요'),
     })
-    .refine(({ password, confirmPassword }) => password === confirmPassword, {
-      path: ['confirmPassword'],
-      message: '패스워드가 일치하지 않습니다.',
-    })
-);
+  )
+  .refine(data => data.password === data.confirmPassword, {
+    message: '패스워드가 일치하지 않습니다',
+    path: ['confirmPassword'],
+  });
 
 export { signInSchema, signUpSchema };
