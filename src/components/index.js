5efa76b3b60002ed960fa0faa27512da692ae46e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-24 10:25:05 +0900

    Feat: components index 내 Signup 관련 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,11 @@ export { default as ThemeButton } from './ThemeButton';
 
 export { default as Header } from './Header';
 export { default as Content } from './Content';
+
 export { default as SideNavBar } from './SideNavBar';
 export { default as SigninForm } from './SigninForm';
+export { default as SignupHeader } from './SignupHeader';
+export { default as SignupForm } from './SignupForm';
+export { default as SignupOption } from './SignupOption';
 
 export { default as SearchBar } from './SearchBar';
