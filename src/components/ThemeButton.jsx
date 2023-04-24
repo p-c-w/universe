90d26c8412b093a03ea2ecaff0bea8c63e670bc5
5kdk <tylerk1121@gmail.com>
2023-04-24 10:21:45 +0900

    Design: 버튼 스타일 변경

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -4,20 +4,19 @@ import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
 const Button = styled(ActionIcon)`
   position: fixed;
-  right: 1.875rem;
-  bottom: 1.875rem;
+  right: 1rem;
+  bottom: 1rem;
 `;
 
 const ThemeButton = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-
   const dark = colorScheme === 'dark';
 
   return (
     <Button
       size="xl"
       variant="outline"
-      color={dark ? 'gray' : 'dark'}
+      color={dark ? 'yellow' : 'violet'}
       onClick={() => toggleColorScheme()}
       title="Toggle color scheme">
       {dark ? <IconSun size="1.2rem" /> : <IconMoonStars size="1.2rem" />}
