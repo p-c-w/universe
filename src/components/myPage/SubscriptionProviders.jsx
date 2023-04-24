c6ad2d7a21ed9b643fe4e588285c365a444e3437
Inae Choi <inaechoi89@gmail.com>
2023-04-25 02:22:44 +0900

    Rename: 파일명 수정
    - service 를 provider로 수정

--- src/components/myPage/SubscriptionProviders.jsx
+++ src/components/myPage/SubscriptionProviders.jsx
@@ -1,12 +1,12 @@
 import { Container, Title } from '@mantine/core';
-import SubscribeBadges from './SubscribeBadges';
+import ProviderBadges from './ProviderBadges';
 
 const SubscriptionServices = () => (
   <Container mt={10} p={0}>
     <Title order={5} mb={10} fw={400}>
       현재 구독하고 있어요
     </Title>
-    <SubscribeBadges />
+    <ProviderBadges />
   </Container>
 );
 
