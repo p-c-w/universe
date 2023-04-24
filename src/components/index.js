4b97c4c4dc620f287015908b76dfef3f08be8916
5kdk <tylerk1121@gmail.com>
2023-04-24 10:25:28 +0900

    Feat: GlobalShell 컴포넌트 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as GlobalShell } from './GlobalShell';
 export { default as ThemeButton } from './ThemeButton';
 export { default as ListButton } from './ListButton';
 export { default as MyList } from './MyList';
@@ -5,7 +6,5 @@ export { default as Badge } from './Badge';
 export { default as Badges } from './Badges';
 export { default as ListBadges } from './ListBadges';
 export { default as Header } from './Header';
-export { default as Content } from './Content';
 export { default as SideNavBar } from './SideNavBar';
 export { default as SearchBar } from './SearchBar';
-
