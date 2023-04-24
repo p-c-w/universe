cbf18ae37997c3c91dbbdf714631a1e3889b7970
5kdk <tylerk1121@gmail.com>
2023-04-24 10:20:21 +0900

    Refactor: mantine UI를 활용한 컴포넌트로 변경

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,42 @@
 import styled from '@emotion/styled';
-import { Input, Tooltip } from '@mantine/core';
-import { IconSearch, IconAlertCircle } from '@tabler/icons-react';
+import { TextInput, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSearch, IconArrowRight } from '@tabler/icons-react';
 
 const Container = styled.div`
-  width: 100%;
   margin: 0 1.875rem;
+  width: 100%;
 `;
 
-const Bar = styled(Input)`
-  position: absolute;
-  width: 37.5rem;
-  top: 1.25rem;
-  left: 50%;
-  transform: translate(-50%, 0);
-  border: 5px;
+const InputWrapper = styled.div`
+  width: 500px;
+  margin: 0 auto;
 `;
 
-const SearchBar = () => (
-  <Container>
-    <Bar
-      icon={<IconSearch size="1rem" />}
-      placeholder="Search"
-      rightSection={
-        <Tooltip label="This is public" position="top-end" withArrow>
-          <div>
-            <IconAlertCircle size="1rem" style={{ display: 'block', opacity: 0.5 }} />
-          </div>
-        </Tooltip>
-      }
-    />
-  </Container>
-);
+const Input = styled(TextInput)``;
+
+const SearchBar = () => {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  return (
+    <Container>
+      <InputWrapper>
+        <Input
+          icon={<IconSearch size="1.1rem" stroke={1.5} />}
+          radius="xl"
+          size="sm"
+          color={dark ? 'violet' : 'dark'}
+          rightSection={
+            <ActionIcon size="sm" radius="xl" color={dark ? 'dark' : 'gray'} variant={dark ? 'filled' : 'light'}>
+              <IconArrowRight size="1.1rem" stroke={1.5} />
+            </ActionIcon>
+          }
+          placeholder="Search for a movie, tv show"
+          rightSectionWidth={42}
+        />
+      </InputWrapper>
+    </Container>
+  );
+};
 
 export default SearchBar;
