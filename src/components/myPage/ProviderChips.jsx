1d618366cec3a2595dd3ebf4bdc2b5afb43f34ab
Inae Choi <inaechoi89@gmail.com>
2023-04-25 03:02:31 +0900

    Feat: 구독중 서비스 컴포넌트내 제목 및 edit버튼 추가

--- src/components/myPage/ProviderChips.jsx
+++ src/components/myPage/ProviderChips.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react';
-import { Chip, Flex } from '@mantine/core';
+import { Chip, Flex, Title, ActionIcon } from '@mantine/core';
+import { IconDiscountCheck } from '@tabler/icons-react';
 import PROVIDERS from '../../constants/provides';
 
 const ServiceChips = () => {
   const [value, setValue] = useState(['Watcha', 'Wavve']);
 
   return (
-    <Flex gap={3} wrap="wrap">
+    <>
+      <Flex justify="space-between" align="center" mb={10}>
+        <Title order={5} fw={400}>
+          구독중인 서비스를 선택해주세요.
+        </Title>
+        <ActionIcon variant="transparent" align="center">
+          <IconDiscountCheck size="1rem" />
+        </ActionIcon>
+      </Flex>
       <Chip.Group multiple value={value} onChange={setValue}>
-        {PROVIDERS.map(provider => (
-          <Chip key={provider.id} value={provider.provider_name}>
-            {provider.provider_name}
-          </Chip>
-        ))}
+        <Flex gap={3} wrap="wrap">
+          {PROVIDERS.map(provider => (
+            <Chip key={provider.id} value={provider.provider_name}>
+              {provider.provider_name}
+            </Chip>
+          ))}
+        </Flex>
       </Chip.Group>
-    </Flex>
+    </>
   );
 };
 
--- src/components/myPage/SubscriptionProviders.jsx
+++ src/components/myPage/SubscriptionProviders.jsx
@@ -1,11 +1,17 @@
-import { Container, Title } from '@mantine/core';
+import { Container, Title, Flex, ActionIcon } from '@mantine/core';
+import { IconSettings } from '@tabler/icons-react';
 import ProviderBadges from './ProviderBadges';
 
 const SubscriptionServices = () => (
   <Container mt={10} p={0}>
-    <Title order={5} mb={10} fw={400}>
-      현재 구독하고 있어요
-    </Title>
+    <Flex justify="space-between" align="center" mb={10}>
+      <Title order={5} fw={400}>
+        현재 구독하고 있어요
+      </Title>
+      <ActionIcon variant="transparent">
+        <IconSettings size="1rem" />
+      </ActionIcon>
+    </Flex>
     <ProviderBadges variant={'dot'} />
   </Container>
 );
