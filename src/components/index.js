8294d919a9ef70777ac3b96a3604e0095351f22b
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-24 11:07:51 +0900

    fix: 병합 처리 오류 코드 수정

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,6 @@ export { default as Badge } from './Badge';
 export { default as Badges } from './Badges';
 export { default as ListBadges } from './ListBadges';
 export { default as Header } from './Header';
-export { default as Content } from './Content';
 export { default as SideNavBar } from './SideNavBar';
 export { default as SigninForm } from './SigninForm';
 export { default as SignupHeader } from './SignupHeader';
