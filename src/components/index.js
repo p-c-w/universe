9b71fb19fae5e83bda755cf92c767dffb575c0ca
5kdk <tylerk1121@gmail.com>
2023-04-25 21:37:40 +0900

    Fix: index js 미사용 export문 제거

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,3 @@
-export { default as ListButton } from './ListButton';
-export { default as MyList } from './MyList';
 export { default as Badge } from './Badge';
 export { default as Badges } from './Badges';
 export { default as CollectionButtons } from './CollectionButtons';
-export { default as ListBadges } from './ListBadges';
-export { default as Header } from './Header';
-export { default as SideNavBar } from './SideNavBar';
-export { default as SearchBar } from './SearchBar';
