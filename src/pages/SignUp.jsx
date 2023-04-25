c6970854853bd865a2912d110ee8d402bde80705
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-25 10:45:54 +0900

    Refactor: active 상태의 초기 값 변경

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,14 +4,14 @@ import { Flex, Container } from '@mantine/core';
 import { SignupHeader, SignupForm, SignupOption } from '../components/auth';
 
 const SignUp = () => {
-  const [active, setActive] = useState(1);
+  const [active, setActive] = useState('form');
 
   return (
     <>
       <SignupHeader />
       <Flex mih={500} justify="center" direction="column" wrap="wrap">
         <Container style={{ width: '600px', padding: '20px', border: '1px solid #373a40', borderRadius: '6px' }}>
-          {active === 1 ? <SignupForm setActive={setActive} /> : <SignupOption />}
+          {active === 'form' ? <SignupForm setActive={setActive} /> : <SignupOption />}
         </Container>
       </Flex>
     </>
