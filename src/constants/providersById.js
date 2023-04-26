ca8d20dece1e70721c5b60e823a4104e5ef6719e
Inae Choi <inaechoi89@gmail.com>
2023-04-27 03:07:35 +0900

    Feat: PROVIDERS  상수에 요금 추가

--- src/constants/providersById.js
+++ src/constants/providersById.js
@@ -3,31 +3,37 @@ const PROVIDERS = [
     id: 8,
     provider_name: 'Netflix',
     providerImgPath: 'assets/badges/netflix.svg',
+    fee: 9500,
   },
   {
     id: 119,
     provider_name: 'Amazon Prime Video',
     providerImgPath: 'assets/badges/primevideo.svg',
+    fee: 7900,
   },
   {
     id: 337,
     provider_name: 'Disney Plus',
     providerImgPath: 'assets/badges/disneyplus.svg',
+    fee: 9900,
   },
   {
     id: 356,
     provider_name: 'Wavve',
     providerImgPath: 'assets/badges/wavve.svg',
+    fee: 7900,
   },
   {
     id: 97,
     provider_name: 'Watcha',
     providerImgPath: 'assets/badges/watcha.svg',
+    fee: 7900,
   },
   {
     id: 350,
     provider_name: 'Apple TV Plus',
     providerImgPath: 'assets/badges/appletvplus.svg',
+    fee: 6500,
   },
 ];
 
