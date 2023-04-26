6a371c52806f431d5759da6e998ef3253ecbd575
5kdk <tylerk1121@gmail.com>
2023-04-26 09:56:12 +0900

    Fix: 쿼리클라이언트 기본 옵션 제거

--- src/hooks/useMainBoardQuery.js
+++ src/hooks/useMainBoardQuery.js
@@ -4,6 +4,8 @@ import { fetchMediaContentTrendings } from '../api/tmdb';
 const useMainBoardQuery = mediaType => {
   const { data, isSuccess, hasNextPage, fetchNextPage } = useInfiniteQuery({
     queryKey: [`@${mediaType}`],
+    suspense: true,
+    staleTime: 1000 * 60 * 5,
     queryFn: ({ pageParam = 1 }) => fetchMediaContentTrendings(mediaType, pageParam),
     getNextPageParam: (lastPage, allPages) =>
       lastPage.pages !== lastPage.total_pages ? allPages.length + 1 : undefined,
