5990b97cb43b2219cd72a3acd63e41474ad01822
Inae Choi <inaechoi89@gmail.com>
2023-04-26 19:23:09 +0900

    Feat: collection내의 title, modified_at data api 연결

--- src/components/myPage/Collection.jsx
+++ src/components/myPage/Collection.jsx
@@ -1,79 +1,10 @@
 import { useRef } from 'react';
 import { Group, Text, Accordion } from '@mantine/core';
+import { useQueries } from '@tanstack/react-query';
 import { useRecoilValue } from 'recoil';
 import { Badge, CollectionButtons } from '../index';
+import { fetchMediaContentDetails } from '../../api';
 import userState from '../../recoil/atom/userState';
-
-// mockdata
-const users = [
-  {
-    email: 'snowlover@gmail.com',
-    password: 'snow123',
-    name: 'snowlover',
-    subscribe_list: [{ id: 8, price: 7900 }],
-    like_list: [
-      { id: 507129, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
-      { id: 37692, type: 'tv', modified_at: '2020-12-31T12:59:32.746Z' },
-    ],
-    watch_list: [{ id: 18465, type: 'movie', modified_at: '2021-05-18T12:59:32.746Z' }],
-    history_list: [
-      { id: 829, type: 'movie', modified_at: '2023-04-15T12:59:32.746Z' },
-      { id: 54823, type: 'tv', modified_at: '2022-01-01T12:59:32.746Z' },
-    ],
-  },
-];
-
-const mockData = [
-  {
-    id: '507129', // user
-    providerImg: 'assets/badges/appletvplus.svg', // tmdb
-    title: '모범택시', // tmdb
-    contentImg: 'https://image.tmdb.org/t/p/w300/xHB43aUOtIezsD0lUzA0Sqk7ALr.jpg', // tmdb
-    modified_at: '2021-05-18T12:59:32.746Z', // user
-  },
-  {
-    id: '37692',
-    providerImg: 'assets/badges/disneyplus.svg',
-    title: '기생충',
-    contentImg: 'https://image.tmdb.org/t/p/w300/jjHccoFjbqlfr4VGLVLT7yek0Xn.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-  {
-    id: '54823',
-    providerImg: 'assets/badges/wavve.svg',
-    title: 'Avengers',
-    contentImg: 'https://image.tmdb.org/t/p/w300/1uHRkB2Q00Y4i7I7KNd0jGi4OmY.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-  {
-    id: '1',
-    providerImg: 'assets/badges/wavve.svg',
-    title: 'Avengers',
-    contentImg: 'https://image.tmdb.org/t/p/w300/1uHRkB2Q00Y4i7I7KNd0jGi4OmY.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-  {
-    id: '2',
-    providerImg: 'assets/badges/wavve.svg',
-    title: 'Avengers',
-    contentImg: 'https://image.tmdb.org/t/p/w300/1uHRkB2Q00Y4i7I7KNd0jGi4OmY.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-  {
-    id: '3',
-    providerImg: 'assets/badges/wavve.svg',
-    title: 'Avengers',
-    contentImg: 'https://image.tmdb.org/t/p/w300/1uHRkB2Q00Y4i7I7KNd0jGi4OmY.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-  {
-    id: '4',
-    providerImg: 'assets/badges/wavve.svg',
-    title: 'Avengers',
-    contentImg: 'https://image.tmdb.org/t/p/w300/1uHRkB2Q00Y4i7I7KNd0jGi4OmY.jpg',
-    modified_at: '2021-05-18T12:59:32.746Z',
-  },
-];
 
 const getAddedDate = modifiedAt => modifiedAt.match(/^([a-zA-Z0-9_.+-]+)T/)[1].replace(/-/g, ' .');
 
@@ -89,12 +20,35 @@ const AccordionLabel = ({ title, providerImg }) => (
 
 const Collection = ({ category, setSelected, setImgSrc }) => {
   const user = useRecoilValue(userState);
-  const userCollection = user[`${category.toLowerCase()}_list`];
+  const userCollectionList = user[`${category.toLowerCase()}_list`];
+
+  const queries = userCollectionList?.map(item => ({
+    queryKey: ['@collection', item],
+    queryFn: () => fetchMediaContentDetails(item.type, item.id),
+    select: item => ({ id: item.id, title: item.title || item.name, posterPath: item.poster_path }),
+    suspense: true,
+  }));
+
+  const detailDatas = useQueries({
+    queries,
+  }).map(query => query.data);
+
+  let collection = [];
+
+  collection = userCollectionList
+    .map(item => ({
+      id: item.id,
+      modified_at: item.modified_at,
+    }))
+    .map(item => {
+      const detailData = detailDatas.find(data => data.id === item.id);
+      return { ...item, ...detailData };
+    });
 
   const itemRef = useRef(null);
 
-  const items = mockData.map(item => (
-    <Accordion.Item value={item.id} key={item.id}>
+  const items = collection.map(item => (
+    <Accordion.Item value={item.title} key={item.id}>
       <Accordion.Control>
         <AccordionLabel {...item} />
       </Accordion.Control>
@@ -110,7 +64,7 @@ const Collection = ({ category, setSelected, setImgSrc }) => {
   const handleChange = e => {
     itemRef.current = e;
     setSelected(itemRef.current !== null);
-    setImgSrc(mockData.find(item => item.id === e)?.contentImg);
+    setImgSrc(collection.find(item => item.title === e)?.posterPath);
   };
 
   return (
