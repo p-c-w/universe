673b5ada9a597a241f6a2431e3657d1f25ff8ae0
Inae Choi <inaechoi89@gmail.com>
2023-04-26 23:40:45 +0900

    Feat: 상수 PROVIDERS에 이미지 경로 추가

--- src/constants/providersById.js
+++ src/constants/providersById.js
@@ -2,26 +2,32 @@ const PROVIDERS = [
   {
     id: 8,
     provider_name: 'Netflix',
+    providerImgPath: 'assets/badges/netflix.svg',
   },
   {
     id: 119,
     provider_name: 'Amazon Prime Video',
+    providerImgPath: 'assets/badges/primevideo.svg',
   },
   {
     id: 337,
     provider_name: 'Disney Plus',
+    providerImgPath: 'assets/badges/disneyplus.svg',
   },
   {
     id: 356,
     provider_name: 'Wavve',
+    providerImgPath: 'assets/badges/wavve.svg',
   },
   {
     id: 97,
     provider_name: 'Watcha',
+    providerImgPath: 'assets/badges/watcha.svg',
   },
   {
     id: 350,
     provider_name: 'Apple TV Plus',
+    providerImgPath: 'assets/badges/appletvplus.svg',
   },
 ];
 
