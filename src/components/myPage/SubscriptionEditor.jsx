1c0212d9a1a5862acaee3c9f7ad0069871159f53
Inae Choi <inaechoi89@gmail.com>
2023-04-27 19:44:47 +0900

    Feat: 구독서비스 변경 기능에 useForm 추가

--- src/components/myPage/SubscriptionEditor.jsx
+++ src/components/myPage/SubscriptionEditor.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react';
 import { Chip, Flex, Title, ActionIcon } from '@mantine/core';
 import { IconDiscountCheck } from '@tabler/icons-react';
+import { useForm } from 'react-hook-form';
 import { PROVIDERS } from '../../constants';
 
 const SubscriptionEditor = ({ providers, onClick }) => {
   const providersNames = providers?.map(provider => provider.provider_name);
 
-  const [value, setValue] = useState(providersNames);
+  const [selectedProviders, setSelectedProviders] = useState(providersNames);
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+
+    onClick();
+  };
 
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Flex justify="space-between" align="center" mb={10}>
         <Title order={5} fw={400}>
           구독중인 서비스를 선택해주세요.
         </Title>
-        <ActionIcon variant="transparent" align="center" onClick={onClick}>
+        <ActionIcon component="button" type="submit" variant="transparent" align="center">
           <IconDiscountCheck size="1rem" />
         </ActionIcon>
       </Flex>
-      <Chip.Group multiple value={value} onChange={setValue}>
+      <Chip.Group multiple value={selectedProviders} onChange={setSelectedProviders}>
         <Flex gap={3} wrap="wrap">
           {PROVIDERS.map(provider => (
-            <Chip key={provider.id} value={provider.provider_name}>
+            <Chip key={provider.id} value={provider.provider_name} {...register(`${provider.provider_name}`)}>
               {provider.provider_name}
             </Chip>
           ))}
         </Flex>
       </Chip.Group>
-    </>
+    </form>
   );
 };
 
