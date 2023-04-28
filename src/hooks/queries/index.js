0533a773aefb2793a46e9374348eaaacb0c222d6
5kdk <tylerk1121@gmail.com>
2023-04-28 19:51:19 +0900

    Fix: useSearchResultQuery import 문 제거

--- src/hooks/queries/index.js
+++ src/hooks/queries/index.js
@@ -1,6 +1,5 @@
 export { default as useContentDetailQueries } from './useContentDetailQueries';
 export { default as useProviderQueries } from './useProviderQueries';
-export { default as useSearchResultQuery } from './useSearchResultQuery';
 export { default as useSortByPopularityInfinityQuery } from './useSortByPopularityInfinityQuery';
 export { default as useSortByPopularityQuery } from './useSortByPopularityQuery';
 export { default as useSortByReleaseDateQuery } from './useSortByReleaseDateQuery';
