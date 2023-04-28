55c1595b69696b1b97ab620cb3102a00c5cec7f4
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-28 21:58:13 +0900

    Feat: PRICE 상수 구조 수정

--- src/constants/priceInfo.js
+++ src/constants/priceInfo.js
@@ -1,10 +1,10 @@
 const PRICE = {
-  8: { basic: 9500 },
-  97: { basic: 7900 },
-  119: { basic: 7900 },
-  337: { basic: 9900 },
-  350: { basic: 6500 },
-  356: { basic: 7900 },
+  8: { name: 'Netflix', basic: 9500, path: 'assets/badges/netflix.svg' },
+  97: { name: 'Watcha', basic: 7900, path: 'assets/badges/watcha.svg' },
+  119: { name: 'Amazon Prime Video', basic: 7900, path: 'assets/badges/primevideo.svg' },
+  337: { name: 'Disney Plus', basic: 9900, path: 'assets/badges/disneyplus.svg' },
+  350: { name: 'Apple TV Plus', basic: 6500, path: 'assets/badges/appletvplus.svg' },
+  356: { name: 'Watcha', basic: 7900, path: 'assets/badges/wavve.svg' },
 };
 
 export default PRICE;
