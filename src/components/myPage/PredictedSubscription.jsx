9661aa16eb357d6744be6aebde8ba60a186aca89
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-28 21:36:25 +0900

    Feat: 최저 구독료 추천 기능 추가

--- src/components/myPage/PredictedSubscription.jsx
+++ src/components/myPage/PredictedSubscription.jsx
@@ -1,10 +1,25 @@
 import { Title, Text, Flex, Container, useMantineColorScheme, useMantineTheme } from '@mantine/core';
+import { useRecoilValue } from 'recoil';
 import { Badges } from '../index';
+import userState from '../../recoil/atom/userState';
+
+import { useProviderQueries } from '../../hooks/queries';
+import calculateLowestFee from '../../utils/calculateLowestFee';
 
 const PredictedSubscription = () => {
   const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === 'dark';
   const theme = useMantineTheme();
+
+  const user = useRecoilValue(userState);
+
+  const userCollectionList = user.watch_list.map(list => ({ id: list.id, type: list.type }));
+
+  const { providers } = useProviderQueries(userCollectionList, {
+    select: data => ({ id: data.id, providers: data.results.KR.flatrate ?? [] }),
+  });
+
+  const { cheapestPrice } = calculateLowestFee(providers);
 
   return (
     <Container m={0} p={0}>
@@ -18,7 +33,7 @@ const PredictedSubscription = () => {
         fz="3.5rem"
         color={dark ? theme.colors[theme.primaryColor][2] : theme.colors[theme.primaryColor][9]}
         fw={500}>
-        ₩17,800
+        ₩{cheapestPrice}
       </Text>
     </Container>
   );
