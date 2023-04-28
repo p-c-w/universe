85609fc7be20c64a97015977923b51c98ae9542b
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-28 21:32:00 +0900

    Feat: Price Info 상수 추가

---
+++ src/constants/priceInfo.js
@@ -0,0 +1,10 @@
+const PRICE = {
+  8: { basic: 9500 },
+  97: { basic: 7900 },
+  119: { basic: 7900 },
+  337: { basic: 9900 },
+  350: { basic: 6500 },
+  356: { basic: 7900 },
+};
+
+export default PRICE;
