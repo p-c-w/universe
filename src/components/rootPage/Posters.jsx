161bf8fa3d5aec28abf7bc30d4a396ff57b73079
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-28 16:18:42 +0900

    Fix: 병합 충돌 해결

--- src/components/rootPage/Posters.jsx
+++ src/components/rootPage/Posters.jsx
@@ -13,12 +13,15 @@ import {
   rem,
 } from '@mantine/core';
 import styled from '@emotion/styled';
-import { useCallback } from 'react';
+import { useCallback, useState, Suspense } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+
 import useSortByPopularityInfinityQuery from '../../hooks/queries/useSortByPopularityInfinityQuery';
 import useObsever from '../../hooks/useObsever';
 import { ScrollObserver } from '../common';
 import genres from '../../constants/genres';
 import PosterSkeleton from './PosterSkeleton';
+import DetailModal from '../common/Detail/DetailModal';
 
 const CardGrid = styled(SimpleGrid)`
   position: relative;
@@ -74,11 +77,33 @@ const Footer = styled(Group)`
   flex-direction: column;
 `;
 
-const Poster = ({ title, originalTitle, posterPath, overview, date, genreIds, mediaType }) => {
+const Poster = ({
+  setModalState,
+  open,
+  id,
+  title,
+  originalTitle,
+  posterPath,
+  backdropPath,
+  overview,
+  date,
+  genreIds,
+  mediaType,
+}) => {
   const theme = useMantineTheme();
+  const genre = mediaType === 'movie' ? genres.movie : genres.tv;
+
+  const handlePosterClick = () => {
+    const genreLists = genreIds?.map(id => genre[id].name);
+
+    const data = { id, title, backdropPath, posterPath, overview, genreLists, mediaType };
+
+    setModalState(data);
+    open();
+  };
 
   return (
-    <StyledCard w={252} h={355} radius="md">
+    <StyledCard w={252} h={355} radius="md" onClick={handlePosterClick}>
       <Image
         src={posterPath ? `https://image.tmdb.org/t/p/w342${posterPath}` : 'https://placehold.co/252x378?text=TDB'}
       />
@@ -130,6 +155,17 @@ const observeOption = { rootMargin: '50%' };
 const Posters = ({ mediaType }) => {
   const { isSuccess, data: content, hasNextPage, fetchNextPage } = useSortByPopularityInfinityQuery(mediaType);
 
+  const [opened, { open, close }] = useDisclosure(false);
+  const [modalState, setModalState] = useState({
+    id: '',
+    title: '',
+    backgroundPath: '',
+    posterPath: '',
+    overview: '',
+    genreLists: [],
+    mediaType: '',
+  });
+
   const getNextPage = useCallback(() => {
     if (hasNextPage) fetchNextPage();
   }, [hasNextPage, fetchNextPage]);
@@ -158,16 +194,21 @@ const Posters = ({ mediaType }) => {
               original_title: originalTitle,
               original_name: originalName,
               poster_path: posterPath,
+              backdrop_path: backdropPath,
               genre_ids: genreIds,
               overview,
               release_date: releaseDate,
               first_air_date: firstAirDate,
             }) => (
               <Poster
+                open={open}
+                setModalState={setModalState}
                 key={id}
+                id={id}
                 title={movie ? title : name}
                 originalTitle={movie ? originalTitle : originalName}
                 posterPath={posterPath}
+                backdropPath={backdropPath}
                 genreIds={genreIds}
                 overview={overview}
                 date={movie ? releaseDate : firstAirDate}
@@ -176,6 +217,14 @@ const Posters = ({ mediaType }) => {
             )
           )}
       </CardGrid>
+      {opened && (
+        <Suspense
+          fallback={
+            <div style={{ position: 'absolute', width: '100px', height: '200px', backgroundColor: '#fff' }}></div>
+          }>
+          <DetailModal opened={opened} close={close} movie={modalState} />
+        </Suspense>
+      )}
       {hasNextPage && <ScrollObserver skeleton={<PosterSkeleton />} hasNextPage={hasNextPage} observer={observerRef} />}
     </>
   );
