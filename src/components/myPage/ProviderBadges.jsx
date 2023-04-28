86eb843ef59695149343066bd3b693dbdcef568b
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-04-28 21:38:15 +0900

    Refactor: 불필요한 콘솔 제거

--- src/components/myPage/ProviderBadges.jsx
+++ src/components/myPage/ProviderBadges.jsx
@@ -1,19 +1,16 @@
 import { Flex, Badge } from '@mantine/core';
 import { PROVIDERS } from '../../constants';
 
-const ProviderBadges = ({ providers = PROVIDERS, variant }) => {
-  console.log(providers);
-  return (
-    <>
-      <Flex gap={3} wrap="wrap">
-        {providers?.map(provider => (
-          <Badge key={provider.id} size="lg" variant={variant}>
-            {provider.provider_name}
-          </Badge>
-        ))}
-      </Flex>
-    </>
-  );
-};
+const ProviderBadges = ({ providers = PROVIDERS, variant }) => (
+  <>
+    <Flex gap={3} wrap="wrap">
+      {providers?.map(provider => (
+        <Badge key={provider.id} size="lg" variant={variant}>
+          {provider.provider_name}
+        </Badge>
+      ))}
+    </Flex>
+  </>
+);
 
 export default ProviderBadges;
