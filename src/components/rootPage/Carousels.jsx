a985cb6fbb71c920790d4d92495dc8e72994cca1
5kdk <tylerk1121@gmail.com>
2023-04-28 19:29:07 +0900

    Refactor: Carousel 컴포넌트 분리

--- src/components/rootPage/Carousels.jsx
+++ src/components/rootPage/Carousels.jsx
@@ -1,13 +1,9 @@
-import { useState, useCallback } from 'react';
-import { Carousel } from '@mantine/carousel';
-import { Container, Title } from '@mantine/core';
+import { useState, useCallback, Suspense } from 'react';
 import { ScrollObserver } from '../common';
-import useSortByPopularityQuery from '../../hooks/queries/useSortByPopularityQuery';
-import useSortByReleaseDateQuery from '../../hooks/queries/useSortByReleaseDateQuery';
-import { CarouselSkeleton, Slide } from '.';
-import useObsever from '../../hooks/useObsever';
+import { CarouselSkeleton, CarouselWithTitle } from '.';
+import { useSortByPopularityQuery, useSortByReleaseDateQuery, useWithGenreQuery } from '../../hooks/queries';
+import useObserver from '../../hooks/useObserver';
 import genres from '../../constants/genres';
-import CarouselWithTitle from './CarouselWithTitle';
 
 const observeOption = { rootMargin: '50%' };
 
@@ -15,8 +11,6 @@ const Carousels = ({ mediaType, providerIds }) => {
   const genreKeys = Object.keys(genres[mediaType]);
   const [step, setStep] = useState(0);
   const [genreIds, setGenreIds] = useState([]);
-  const { isSuccess: popular, data: popularContent } = useSortByPopularityQuery(mediaType, providerIds);
-  const { isSuccess: recent, data: recentContent } = useSortByReleaseDateQuery(mediaType, providerIds);
 
   const remainGenre = genreKeys.length !== genreIds.length;
 
@@ -25,66 +19,34 @@ const Carousels = ({ mediaType, providerIds }) => {
     setStep(step => step + 1);
   }, [genreKeys, step]);
 
-  const observerRef = useObsever(getNextStep, observeOption);
+  const observerRef = useObserver(getNextStep, remainGenre, observeOption);
 
-  const isMovie = mediaType === 'movie';
+  const topTitles = ['인기작', '최신작'];
 
   return (
     <>
-      <Container maw={'none'} p={0} py={'md'}>
-        <Title fz={'md'}>{'인기작품'}</Title>
-        <Carousel slideSize="20%" mt="md" slideGap="md" loop align="start" slidesToScroll={6} dragFree>
-          {popular &&
-            popularContent?.map(
-              ({
-                id,
-                title,
-                name,
-                original_title: originalTitle,
-                original_name: originalName,
-                backdrop_path: backdropPath,
-              }) => (
-                <Slide
-                  key={id}
-                  title={isMovie ? title : name}
-                  originalTitle={isMovie ? originalTitle : originalName}
-                  name={name}
-                  backdropPath={backdropPath}
-                  mediaType={mediaType}
-                />
-              )
-            )}
-        </Carousel>
-      </Container>
-      <Container maw={'none'} p={0} py={'md'}>
-        <Title fz={'md'}>{'최신작'}</Title>
-        <Carousel slideSize="20%" mt={'md'} slideGap="md" loop align="start" slidesToScroll={6} dragFree>
-          {recent &&
-            recentContent?.map(
-              ({
-                id,
-                title,
-                name,
-                original_title: originalTitle,
-                original_name: originalName,
-                backdrop_path: backdropPath,
-              }) => (
-                <Slide
-                  key={id}
-                  title={isMovie ? title : name}
-                  originalTitle={isMovie ? originalTitle : originalName}
-                  name={name}
-                  backdropPath={backdropPath}
-                  mediaType={mediaType}
-                />
-              )
-            )}
-        </Carousel>
-      </Container>
-      {genreIds.map(genreId => (
-        <CarouselWithTitle key={genreId} mediaType={mediaType} genreId={genreId} providerIds={providerIds} />
+      {topTitles.map(title => (
+        <Suspense key={title} fallback={<CarouselSkeleton />}>
+          <CarouselWithTitle
+            mediaType={mediaType}
+            providerIds={providerIds}
+            title={title}
+            fetchFn={title === '최신작' ? useSortByPopularityQuery : useSortByReleaseDateQuery}
+          />
+        </Suspense>
       ))}
-      {remainGenre && <ScrollObserver skeleton={<CarouselSkeleton />} observer={observerRef} />}
+      {genreIds.length !== 0 &&
+        genreIds.map(genreId => (
+          <Suspense key={genreId} fallback={<CarouselSkeleton />}>
+            <CarouselWithTitle
+              mediaType={mediaType}
+              providerIds={providerIds}
+              genreId={genreId}
+              fetchFn={useWithGenreQuery}
+            />
+          </Suspense>
+        ))}
+      <ScrollObserver loader={<CarouselSkeleton />} hasNextPage={remainGenre} observer={observerRef} />
     </>
   );
 };
