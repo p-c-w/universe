f1931ad4e482261f9fca382436e6eb9000281de1
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-04-30 00:07:51 +0900

    refactor: data structure 변경으로 인한 함수 수정

--- src/utils/getProvidersInfoListByList.js
+++ src/utils/getProvidersInfoListByList.js
@@ -1,5 +1,12 @@
-import { PROVIDERS } from '../constants';
+// import { PROVIDERS } from '../constants';
+import PROVIDERS from '../constants/providers';
 
-const getProvidersInfoListByList = list => list?.map(item => PROVIDERS.find(PROVIDER => PROVIDER.id === item.id));
+const getProvidersInfoListByList = list =>
+  list?.map(({ id, price }) => ({
+    id,
+    provider_name: PROVIDERS[id].provider_name,
+    providerImgPath: PROVIDERS[id].provider_name,
+    fee: PROVIDERS[id].fee[price],
+  }));
 
 export default getProvidersInfoListByList;
