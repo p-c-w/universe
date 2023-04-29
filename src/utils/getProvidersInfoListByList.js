3b317b83a8b6e326eb6dd6f78b54c95fb94142ba
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-04-30 01:57:59 +0900

    Fix: key값 오류 수정

--- src/utils/getProvidersInfoListByList.js
+++ src/utils/getProvidersInfoListByList.js
@@ -5,7 +5,7 @@ const getProvidersInfoListByList = list =>
   list?.map(({ id, price }) => ({
     id,
     provider_name: PROVIDERS[id].provider_name,
-    providerImgPath: PROVIDERS[id].provider_name,
+    providerImgPath: PROVIDERS[id].providerImgPath,
     fee: PROVIDERS[id].fee[price],
   }));
 
