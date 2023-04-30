7e191af60e0a028de2e99fd7a212f896ee285798
5kdk <tylerk1121@gmail.com>
2023-04-30 09:29:24 +0900

    Refactor: 전역상태 => 서버상태(리액트 쿼리)

--- src/components/common/ActionIcons.jsx
+++ src/components/common/ActionIcons.jsx
@@ -1,36 +1,52 @@
 import React from 'react';
 import { Group, ActionIcon } from '@mantine/core';
-
 import { IconHeart, IconHistory, IconMovie } from '@tabler/icons-react';
-import axios from 'axios';
 import { useRecoilValue } from 'recoil';
-import userState from '../../recoil/atom/userState';
+import { useUserQuery } from '../../hooks/queries';
+import { useAddUserContentMutation } from '../../hooks/mutations';
+import { userState } from '../../recoil/atom';
 
 const ActionIcons = ({ size, id, type }) => {
-  const { email, watch_list: watchlist, like_list: likelist, history_list: historylist } = useRecoilValue(userState);
-  const now = new Date();
+  const { email } = useRecoilValue(userState);
 
-  // api 요청 확인용 함수
-  const handleClick = async listName => {
-    await axios.patch(`/api/users/${email}/${listName}`, { id, type, modified_at: now.toISOString() });
+  const { data } = useUserQuery({
+    select: userInfo => ({
+      watchlist: userInfo.watch_list,
+      likelist: userInfo.like_list,
+      historylist: userInfo.history_list,
+    }),
+  });
+
+  const { watchlist, likelist, historylist } = data || {
+    watchlist: [],
+    likelist: [],
+    historylist: [],
+  };
+
+  const { mutate: updateContent } = useAddUserContentMutation();
+
+  const handleClick = list => {
+    if (!email) return;
+    const now = new Date();
+    updateContent({ email, list, value: { id, type, modified_at: now.toISOString() } });
   };
 
   return (
     <Group spacing={8}>
       <ActionIcon
-        variant={watchlist.find(({ id: _id, type: _type }) => _id === id && _type === type) ? 'filled' : 'outline'}
+        variant={watchlist?.find(({ id: _id, type: _type }) => +_id === id && _type === type) ? 'filled' : 'outline'}
         color={'yellow'}
         onClick={() => handleClick('watch_list')}>
         <IconMovie size={size} />
       </ActionIcon>
       <ActionIcon
-        variant={!likelist.find(({ id: _id, type: _type }) => _id === id && _type === type) ? 'filled' : 'outline'}
+        variant={likelist?.find(({ id: _id, type: _type }) => _id === id && _type === type) ? 'filled' : 'outline'}
         color={'red'}
         onClick={() => handleClick('like_list')}>
         <IconHeart size={size} />
       </ActionIcon>
       <ActionIcon
-        variant={historylist.find(({ id: _id, type: _type }) => _id === id && _type === type) ? 'filled' : 'outline'}
+        variant={historylist?.find(({ id: _id, type: _type }) => _id === id && _type === type) ? 'filled' : 'outline'}
         onClick={() => handleClick('history_list')}
         color={'blue'}>
         <IconHistory size={size} />
