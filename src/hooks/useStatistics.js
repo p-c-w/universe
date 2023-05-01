7df0dfbdd5c32f470e27b940e46cc429058ab670
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 02:42:58 +0900

    Feat: 통계데이터 atom에 total 값 적용하는 기능 구현

--- src/hooks/useStatistics.js
+++ src/hooks/useStatistics.js
@@ -5,9 +5,11 @@ import { statisticByProviderState } from '../recoil/atom';
 
 const useStatistics = () => {
   const setStatisticData = useSetRecoilState(statisticByProviderState);
-
   const { data } = useUserQuery({ select: userInfo => userInfo.history_list });
   const historyList = data || [];
+  const total = historyList.length;
+
+  setStatisticData(state => ({ ...state, total }));
 };
 
 export default useStatistics;
