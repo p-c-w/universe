8295c5801d2163f66b09f782f996ddd403599efc
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-01 22:32:56 +0900

    Feat: useQueries를 이용하기 위해 파일 분리 및 데이터 구조 수정

--- src/components/myPage/PredictedSubscription.jsx
+++ src/components/myPage/PredictedSubscription.jsx
@@ -1,47 +1,48 @@
-import { Title, Text, Flex, Container, useMantineColorScheme, useMantineTheme } from '@mantine/core';
+import React from 'react';
+import { Container, Title, Text, Flex, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { Badges } from '../index';
-import { useUserQuery, useProviderQueries } from '../../hooks/queries';
+
+import { useProviderQueries } from '../../hooks/queries';
+import PROVIDERS from '../../constants/providers';
 
 import calculateLowestFee from '../../utils/calculateLowestFee';
 
-const PredictedSubscription = () => {
+const PredictedSubscription = ({ watchlist, userCollectionList }) => {
   const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === 'dark';
   const theme = useMantineTheme();
 
-  const { data } = useUserQuery({
-    select: userInfo => ({
-      watchlist: userInfo.watch_list,
+  const queries = useProviderQueries(userCollectionList, {
+    select: data => ({
+      id: data.id,
+      providers: data.results.KR.flatrate
+        ?.map(provider => provider.provider_id)
+        ?.filter(id => Object.prototype.hasOwnProperty.call(PROVIDERS, id)),
     }),
+    enabled: !!watchlist.length,
   });
 
-  const { watchlist } = data || {
-    watchlist: [],
-  };
-
-  const userCollectionList = watchlist.map(list => ({ id: list.id, type: list.type }));
-
-  const { providers } = useProviderQueries(userCollectionList, {
-    select: data => ({ id: data.id, providers: data.results.KR.flatrate ?? [] }),
-  });
+  const providers = queries.map(({ data }) => data).filter(({ providers }) => providers !== undefined);
 
   const { cheapestCombo, cheapestPrice } = calculateLowestFee(providers);
 
   return (
-    <Container m={0} p={0}>
-      <Flex align="center" gap={20}>
-        <Title order={2} size={30} align="left">
-          예상 구독료
-        </Title>
-        <Badges providers={cheapestCombo} size="2rem" />
-      </Flex>
-      <Text
-        fz="3.5rem"
-        color={dark ? theme.colors[theme.primaryColor][2] : theme.colors[theme.primaryColor][9]}
-        fw={500}>
-        ₩{cheapestPrice.toLocaleString()}
-      </Text>
-    </Container>
+    <>
+      <Container m={0} p={0}>
+        <Flex align="center" gap={20}>
+          <Title order={2} size={30} align="left">
+            예상 구독료
+          </Title>
+          <Badges providers={cheapestCombo} size="2rem" />
+        </Flex>
+        <Text
+          fz="3.5rem"
+          color={dark ? theme.colors[theme.primaryColor][2] : theme.colors[theme.primaryColor][9]}
+          fw={500}>
+          ₩{cheapestPrice.toLocaleString()}
+        </Text>
+      </Container>
+    </>
   );
 };
 
