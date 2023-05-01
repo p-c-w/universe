015c16940f2d8b5e14c2f38e487d27ca7722b2bb
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 02:41:40 +0900

    Feat: history list 데이터 가져오기 구현

--- src/hooks/useStatistics.js
+++ src/hooks/useStatistics.js
@@ -1,8 +1,13 @@
 import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useProviderQueries, useUserQuery } from './queries';
+import PROVIDERS from '../constants/providers';
 import { statisticByProviderState } from '../recoil/atom';
 
 const useStatistics = () => {
   const setStatisticData = useSetRecoilState(statisticByProviderState);
+
+  const { data } = useUserQuery({ select: userInfo => userInfo.history_list });
+  const historyList = data || [];
 };
 
 export default useStatistics;
