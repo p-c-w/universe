243eb2282c95c002aeecbfd8edbbc331b4435393
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 02:38:01 +0900

    Feat: 통계데이터 atom 생성

---
+++ src/recoil/atom/statisticByProviderState.js
@@ -0,0 +1,57 @@
+import { atom } from 'recoil';
+import PROVIDERS from '../../constants/providers';
+
+const defaultData = {
+  total: '0',
+  data: [
+    {
+      id: 8,
+      label: 'Netflix',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[8].color}`,
+    },
+    {
+      id: 97,
+      label: 'Watcha',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[97].color}`,
+    },
+    {
+      id: 119,
+      label: 'Amazon Prime',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[119].color}`,
+    },
+    {
+      id: 337,
+      label: 'Disney+',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[337].color}`,
+    },
+    {
+      id: 350,
+      label: 'Apple TV+',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[350].color}`,
+    },
+    {
+      id: 356,
+      label: 'Wavve',
+      count: 0,
+      part: 0,
+      color: `${PROVIDERS[356].color}`,
+    },
+  ],
+};
+
+const statisticByProviderState = atom({
+  key: 'statisticByProviderState',
+  default: defaultData,
+});
+
+export default statisticByProviderState;
