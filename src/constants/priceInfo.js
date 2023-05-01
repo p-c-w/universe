d5a4d633ba5691ab72171bfd0aa9af9d80ed2675
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-01 22:35:11 +0900

    Fix: 잘못된 값 수정

--- src/constants/priceInfo.js
+++ src/constants/priceInfo.js
@@ -4,7 +4,7 @@ const PRICE = {
   119: { name: 'Amazon Prime Video', basic: 7900, path: 'assets/badges/primevideo.svg' },
   337: { name: 'Disney Plus', basic: 9900, path: 'assets/badges/disneyplus.svg' },
   350: { name: 'Apple TV Plus', basic: 6500, path: 'assets/badges/appletvplus.svg' },
-  356: { name: 'Watcha', basic: 7900, path: 'assets/badges/wavve.svg' },
+  356: { name: 'Wavve', basic: 7900, path: 'assets/badges/wavve.svg' },
 };
 
 export default PRICE;
