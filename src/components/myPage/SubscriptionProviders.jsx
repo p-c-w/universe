ed3be58822fd38a6addfe300514b3999f0921e16
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 17:38:42 +0900

    Fix: undefined 오류 방지를 위한 수정

--- src/components/myPage/SubscriptionProviders.jsx
+++ src/components/myPage/SubscriptionProviders.jsx
@@ -6,7 +6,7 @@ const SubscriptionProviders = ({ providers, onClick }) => (
   <Container mt={10} p={0}>
     <Flex justify="space-between" align="center" mb={10}>
       <Title order={5} fw={400}>
-        {providers.length !== 0 ? '현재 구독하고 있어요.' : '현재 구독중인 서비스가 없습니다.'}
+        {providers?.length !== 0 ? '현재 구독하고 있어요.' : '현재 구독중인 서비스가 없습니다.'}
       </Title>
       <ActionIcon variant="transparent" align="center" onClick={onClick} size="sm">
         <IconSettings />
