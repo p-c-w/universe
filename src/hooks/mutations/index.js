6b9b34b6b9754b8a97139f97854b3b5391816af4
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 14:59:39 +0900

    Feat: index.js에 파일 경로 추가

--- src/hooks/mutations/index.js
+++ src/hooks/mutations/index.js
@@ -1,2 +1,3 @@
 export { default as useGenericMutation } from './useGenericMutation';
 export { default as useAddUserContentMutation } from './useAddUserContentMutation';
+export { default as useEditUserNameMutation } from './useEditUserNameMutation';
