736b50544c57e24c4582f4fb0f70b62d86bcedc5
5kdk <tylerk1121@gmail.com>
2023-05-02 20:20:04 +0900

    Design: SignOut UI 변경

--- src/components/auth/Signout.jsx
+++ src/components/auth/Signout.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { IconLogout } from '@tabler/icons-react';
-import { Button } from '@mantine/core';
+import { Text, ThemeIcon, useMantineColorScheme } from '@mantine/core';
 
 import axios from 'axios';
 import { useSetRecoilState } from 'recoil';
+import styled from '@emotion/styled';
 import userState from '../../recoil/atom/userState';
 
-const Signout = ({ size, fontSize }) => {
+const CustomLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: ${({ theme }) => (theme.colorScheme === 'dark' ? theme.colors.gray[0] : theme.colors.gray[9])};
+`;
+
+const Tab = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  list-style: none;
+  height: 2.25rem;
+  padding: 0.3125rem;
+  cursor: pointer;
+  transition: 0.2s ease-in;
+  border-radius: 0.3125rem;
+
+  &:hover {
+    background-color: ${({ theme }) => (theme.colorScheme === 'dark' ? theme.colors.gray[8] : theme.colors.gray[1])};
+  }
+`;
+
+const Signout = () => {
   const setUser = useSetRecoilState(userState);
+  const { colorScheme } = useMantineColorScheme();
+
+  const dark = colorScheme === 'dark';
 
   const handleClick = async () => {
     await axios.get('/api/auth/signout');
@@ -17,21 +44,16 @@ const Signout = ({ size, fontSize }) => {
   };
 
   return (
-    <>
-      <Button
-        bg="none"
-        variant="none"
-        ml={30}
-        w={size}
-        fw={`var(--mantine-font-size-${fontSize})`}
-        display={'flex'}
-        component={Link}
-        to="/"
-        onClick={handleClick}>
-        <IconLogout display={'inline'} />
-        SIGN OUT
-      </Button>
-    </>
+    <Tab role="button" aria-label="label" onClick={handleClick}>
+      <CustomLink to="/">
+        <ThemeIcon variant={dark ? 'filled' : 'light'} color="gray">
+          <IconLogout size="1.1rem" />
+        </ThemeIcon>
+        <Text color={dark ? 'gray.0' : 'gray.9'} ml={25} size="sm">
+          {'Sign out'}
+        </Text>
+      </CustomLink>
+    </Tab>
   );
 };
 
