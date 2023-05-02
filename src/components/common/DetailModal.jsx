0d1d2c5a37960956810c7a570e64d7b2410b5b07
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-02 21:27:47 +0900

    Design: 레이아웃 수정

--- src/components/common/DetailModal.jsx
+++ src/components/common/DetailModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Modal, Image, Grid, Container, Title, Text, Overlay, ScrollArea, Badge } from '@mantine/core';
+import { Modal, Image, Grid, Container, Title, Text, Overlay, ScrollArea, Badge, Flex } from '@mantine/core';
 import styled from '@emotion/styled';
 import Badges from '../Badges';
 import PROVIDERS from '../../constants/providers';
@@ -11,14 +11,12 @@ import { getProvidersByIds } from '../../utils';
 import ActionIcons from './ActionIcons';
 import genres from '../../constants/genres';
 
-const BadgeContainer = styled.div`
-  width: 100%;
-  padding: 0 1.875rem;
-  display: flex;
-  position: absolute;
-  justify-content: space-between;
-  bottom: 1rem;
+const CloseBtn = styled(Modal.CloseButton)`
   z-index: 999;
+`;
+
+const Body = styled(Modal.Body)`
+  z-index: 2;
 `;
 
 const DetailModal = ({
@@ -46,24 +44,30 @@ const DetailModal = ({
       <Modal.Content pos="relative">
         <Overlay c="#000" opacity={0.75} zIndex="1" />
         <Image src={`https://image.tmdb.org/t/p/w780${backdropPath}` || undefined}></Image>
-        <BadgeContainer>
+        <Container>
           <Badges providers={providerIds} spacing="sm" size="2.5rem" />
-          <ActionIcons size={10} id={id} type={type} />
-        </BadgeContainer>
-        <Modal.CloseButton style={{ zIndex: '999' }} pos="absolute" top={10} right={20} />
-        <Modal.Body m={40} c="#fff" style={{ zIndex: '2' }} pos="absolute" top={0}>
+        </Container>
+        <CloseBtn pos="absolute" top={10} right={20} />
+        <Body m={40} c="#fff" pos="absolute" top={0}>
           <Grid columns={5}>
             <Grid.Col span={3}>
               <Container>
-                <Title order={1} mb={10} mt={10}>
+                <Title order={1} mb="xs" mt="xs">
                   {title}
                 </Title>
-                <Text>2023</Text>
-                {genreIds.map(({ id, name }) => (
-                  <Badge color={genres[type][id].color} key={id}>
-                    {name}
-                  </Badge>
-                ))}
+                <Text my="sm" pl={3}>
+                  2023
+                </Text>
+                <Flex justify={'space-between'}>
+                  <Flex maw={200} wrap="wrap" mb="lg">
+                    {genreIds.map(({ id, name }) => (
+                      <Badge color={genres[type][id].color} key={id}>
+                        {name}
+                      </Badge>
+                    ))}
+                  </Flex>
+                  <ActionIcons size={17} id={id} type={type} />
+                </Flex>
                 <ScrollArea fw={300} fz="sm" h={200}>
                   {overview}
                 </ScrollArea>
@@ -75,7 +79,7 @@ const DetailModal = ({
               </Container>
             </Grid.Col>
           </Grid>
-        </Modal.Body>
+        </Body>
       </Modal.Content>
     </>
   );
