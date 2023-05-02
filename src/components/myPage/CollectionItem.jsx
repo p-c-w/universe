db0f02ce889e381fc8f48701e83e4f49fadd0fd0
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-03 07:04:33 +0900

    Feat: CollectionItem 삭제 기능 구현

--- src/components/myPage/CollectionItem.jsx
+++ src/components/myPage/CollectionItem.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, Accordion, Tooltip, Button, ThemeIcon, Flex } from '@mantine/core';
-import { IconLayersLinked } from '@tabler/icons-react';
+import { IconLayersLinked, IconTrash } from '@tabler/icons-react';
+import { useRecoilValue } from 'recoil';
 import { AccordionLabel } from './index';
+import { useDeleteUserContentMutation } from '../../hooks/mutations';
+import { userState, categoryState } from '../../recoil/atom';
 
 const getAddedDate = modifiedAt => modifiedAt?.match(/^([a-zA-Z0-9_.+-]+)T/)[1].replace(/-/g, ' .');
 
 const CollectionItem = ({ item, setClicked, open }) => {
+  const email = useRecoilValue(userState);
+  const listName = useRecoilValue(categoryState);
+
+  const [hovered, setHovered] = useState(false);
+
+  const { mutate: deleteUserContent } = useDeleteUserContentMutation();
+
   const handleClick = item => {
     setClicked(item);
     open();
   };
 
+  const handleMouseEnter = () => setHovered(true);
+
+  const handleMouseLeave = () => setHovered(false);
+
+  const handleTrashClick = e => {
+    e.stopPropagation();
+    deleteUserContent({ email, list: `${listName}_list`, id: item.id });
+  };
+
   return (
     <>
-      <Accordion.Item value={item?.title} key={item?.id}>
+      <Accordion.Item
+        value={item?.title}
+        key={item?.id}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
         <Accordion.Control>
-          <AccordionLabel {...item} />
+          <Flex direction={'row'} justify={'space-between'}>
+            <AccordionLabel {...item} />
+            {hovered && (
+              <ThemeIcon mt={11} variant="transparent" onClick={handleTrashClick}>
+                <IconTrash size={16} />
+              </ThemeIcon>
+            )}
+          </Flex>
         </Accordion.Control>
         <Accordion.Panel w="90%">
           <Flex>
