08d0d40f3d0fa23097648bbcd193be5d25b3b4c9
5kdk <tylerk1121@gmail.com>
2023-05-02 21:46:18 +0900

    Fix: ThemeIcon을 활용한 컴포넌트로 변경

--- src/components/common/ActionIcons.jsx
+++ src/components/common/ActionIcons.jsx
@@ -1,6 +1,7 @@
 import { useRecoilValue } from 'recoil';
-import { Group, ActionIcon } from '@mantine/core';
-import { IconHeart, IconHistory, IconMovie } from '@tabler/icons-react';
+import { Group, ThemeIcon } from '@mantine/core';
+import { IconHistory, IconMovie, IconThumbUp } from '@tabler/icons-react';
+import styled from '@emotion/styled';
 import { userState } from '../../recoil/atom';
 import { useUserQuery } from '../../hooks/queries';
 import { useAddUserContentMutation, useDeleteUserContentMutation } from '../../hooks/mutations';
@@ -11,33 +12,32 @@ const getUserInfo = userInfo => ({
   historylist: userInfo.history_list,
 });
 
-const defaultData = {
-  watchlist: [],
-  likelist: [],
-  historylist: [],
-};
+const CategoryIcon = styled(ThemeIcon)`
+  cursor: pointer;
+`;
 
 const ActionIcons = ({ size, id, type }) => {
-  const email = useRecoilValue(userState);
+  const userEmail = useRecoilValue(userState);
 
-  const { data } = useUserQuery({ select: getUserInfo, enabled: !!email });
+  const { data } = useUserQuery({ select: getUserInfo, enabled: !!userEmail });
 
-  const { mutate: updateUserContent } = useAddUserContentMutation();
+  const { mutate: addUserContent } = useAddUserContentMutation();
   const { mutate: deleteUserContent } = useDeleteUserContentMutation();
 
-  const { watchlist, likelist, historylist } = data || defaultData;
+  const { watchlist = [], likelist = [], historylist = [] } = data || {};
 
   const isItemInList = list => list.some(item => item.id === id && item.type === type);
 
   const handleClick = (list, listName) => {
-    if (!email) return;
+    if (!userEmail) return;
 
-    if (!isItemInList(list)) {
-      const now = new Date();
-      updateUserContent({ email, list: listName, value: { id, type, modified_at: now.toISOString() } });
-    }
+    const now = new Date();
+    const contentData = { id, type, modified_at: now.toISOString() };
+
     if (isItemInList(list)) {
-      deleteUserContent({ email, list: listName, id });
+      deleteUserContent({ email: userEmail, list: listName, id });
+    } else {
+      addUserContent({ email: userEmail, list: listName, value: contentData });
     }
   };
 
@@ -46,15 +46,27 @@ const ActionIcons = ({ size, id, type }) => {
 
   return (
     <Group spacing={8}>
-      <ActionIcon {...getIconVariant(watchlist, 'yellow')} onClick={() => handleClick(watchlist, 'watch_list')}>
+      <CategoryIcon
+        role="button"
+        aria-label={`to ${watchlist}`}
+        {...getIconVariant(watchlist, 'yellow')}
+        onClick={() => handleClick(watchlist, 'watch_list')}>
         <IconMovie size={size} />
-      </ActionIcon>
-      <ActionIcon {...getIconVariant(likelist, 'red')} onClick={() => handleClick(likelist, 'like_list')}>
-        <IconHeart size={size} />
-      </ActionIcon>
-      <ActionIcon {...getIconVariant(historylist, 'blue')} onClick={() => handleClick(historylist, 'history_list')}>
+      </CategoryIcon>
+      <CategoryIcon
+        role="button"
+        aria-label={likelist}
+        {...getIconVariant(likelist, 'red')}
+        onClick={() => handleClick(likelist, 'like_list')}>
+        <IconThumbUp size={size} />
+      </CategoryIcon>
+      <CategoryIcon
+        role="button"
+        aria-label={historylist}
+        {...getIconVariant(historylist, 'blue')}
+        onClick={() => handleClick(historylist, 'history_list')}>
         <IconHistory size={size} />
-      </ActionIcon>
+      </CategoryIcon>
     </Group>
   );
 };
