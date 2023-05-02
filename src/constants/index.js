a292cc72af44d37d6a6ad9f87cf379b12e610345
5kdk <tylerk1121@gmail.com>
2023-05-02 11:12:46 +0900

    Chore: 쿼리키 상수 분리

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,5 @@ export { default as PROVIDER_COLOR } from './providerColor';
 export { default as PRICE } from './priceInfo';
 export { userInfoQueryKey as USER_INFO_QUERY_KEY } from './userQueryKeys';
 export { userContentQueryKey as USER_CONTENT_QUERY_KEY } from './userQueryKeys';
+export { isAuthenticatedQueryKey as IS_AUTHENTICATED_QUERY_KEY } from './userQueryKeys';
+export { searchResultQueryKey as SEARCH_RESULT_QUERY_KEY } from './userQueryKeys';
