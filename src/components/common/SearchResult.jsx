719f4d331538b503375e52a5a1cfef910707fd9f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-02 14:40:15 +0900

    Feat: component 분리

--- src/components/common/SearchResult.jsx
+++ src/components/common/SearchResult.jsx
@@ -1,8 +1,9 @@
 import { forwardRef, useEffect, useRef } from 'react';
-import parse from 'html-react-parser';
 import { Container, Flex, ScrollArea, Space, Text, Title } from '@mantine/core';
 import styled from '@emotion/styled';
 import { useSearchResultQueries } from '../../hooks/queries';
+
+import Result from './Result';
 
 const mediaType = ['movie', 'tv'];
 
@@ -41,32 +42,35 @@ const SearchResult = forwardRef(({ input }, ref) => {
   return (
     <ResultContiner ref={ref} pos="absolute" w={450} px="lg" pt="1.875rem" pb="1.875rem" left="0" c="gray.7" bg="white">
       {searchResults.map((searchResult, idx) => (
-        <Container key={idx}>
-          <Flex align="center" justify="space-between">
-            <Title order={5} tt="uppercase">
-              {mediaType[idx]}
-            </Title>
-          </Flex>
-          <hr />
-          {searchResult.length === 0 ? (
-            <>
+        <>
+          <Container key={idx}>
+            <Flex align="center" justify="space-between">
+              <Title order={5} tt="uppercase">
+                {mediaType[idx]}
+              </Title>
+            </Flex>
+            <hr />
+            {searchResult.length === 0 ? (
               <Text mb="sm" fw={400}>
                 {'검색결과가 없습니다.'}
               </Text>
-            </>
-          ) : (
-            <>
+            ) : (
               <Scroll mah={200} type="always">
                 {searchResult.map(({ id, title, name }) => (
-                  <Text key={id} mb="sm" fw={400}>
-                    {parse((title || name).replace(userInputRegex.current, match => `<b>${match}</b>`))}
-                  </Text>
+                  <Result
+                    key={id}
+                    id={id}
+                    title={title}
+                    name={name}
+                    reg={userInputRegex.current}
+                    type={mediaType[idx]}
+                  />
                 ))}
               </Scroll>
-            </>
-          )}
-          <Space h="md" />
-        </Container>
+            )}
+            <Space h="md" />
+          </Container>
+        </>
       ))}
     </ResultContiner>
   );
