94727aa223d22f36727ab3c0ca68f02a5a04ee90
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-02 14:59:21 +0900

    Feat: useMutation 커스텀훅 사용하여 user name변경 기능 구현

--- src/components/myPage/MypageTitle.jsx
+++ src/components/myPage/MypageTitle.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 import { Title, Button, Container, TextInput } from '@mantine/core';
 import { IconPencil } from '@tabler/icons-react';
 import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
 import { useUserQuery } from '../../hooks/queries';
+import { useEditUserNameMutation } from '../../hooks/mutations';
+import { userState } from '../../recoil/atom';
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -27,7 +30,10 @@ const NameInput = styled(TextInput)`
 `;
 
 const MypageTitle = () => {
+  const email = useRecoilValue(userState);
+
   const { userInfo: name } = useUserQuery({ select: userInfo => userInfo.name });
+  const { mutate: updateUserName } = useEditUserNameMutation();
 
   const [userName, setUserName] = useState(name);
   const [editMode, setEditMode] = useState(false);
@@ -39,6 +45,7 @@ const MypageTitle = () => {
   const handleKeyUp = e => {
     const content = e.target.value.trim();
     if (e.key !== 'Enter' || content === '') return;
+    updateUserName({ email, name: content });
     setEditMode(false);
   };
 
