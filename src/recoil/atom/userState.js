ac83a193f9b40573c84cacce66b502789cb01171
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-02 14:51:39 +0900

    Chore: 불필요한 mock 변수 삭제

--- src/recoil/atom/userState.js
+++ src/recoil/atom/userState.js
@@ -3,25 +3,6 @@ import { updateUser } from '../../api';
 
 const KEY = 'user';
 
-const mock = {
-  email: 'snowlover@gmail.com',
-  password: 'snow123',
-  name: 'snowlover',
-  subscribe_list: [{ id: 8, price: 'basic' }],
-  like_list: [
-    { id: 849869, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
-    { id: 505642, type: 'movie', modified_at: '2020-12-31T12:59:32.746Z' },
-  ],
-  watch_list: [
-    { id: 849869, type: 'movie', modified_at: '2021-12-25T12:59:32.746Z' },
-    { id: 119769, type: 'tv', modified_at: '2020-12-31T12:59:32.746Z' },
-  ],
-  history_list: [
-    { id: 668482, type: 'movie', modified_at: '2023-04-15T12:59:32.746Z' },
-    { id: 61889, type: 'tv', modified_at: '2022-01-01T12:59:32.746Z' },
-  ],
-};
-
 const localStorageEffect = ({ onSet }) => {
   onSet(newState => {
     localStorage.setItem(KEY, JSON.stringify(newState));
@@ -37,7 +18,6 @@ const updateSubcrbieList = ({ onSet }) => {
 const userState = atom({
   key: 'userState',
   default: JSON.parse(localStorage.getItem(KEY)),
-  // default: mock,
   effects: [localStorageEffect, updateSubcrbieList],
 });
 
