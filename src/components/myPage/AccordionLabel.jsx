43b607e3c81df870fa1c345101f89eb3ee7c29f0
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-03 14:55:03 +0900

    Feat: ActionIcons 사용으로 변경

--- src/components/myPage/AccordionLabel.jsx
+++ src/components/myPage/AccordionLabel.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Group, Text } from '@mantine/core';
-import { Badges, CollectionButtons } from '../index';
+import { Box, Group, Text } from '@mantine/core';
+import { useRecoilValue } from 'recoil';
+import { Badges } from '..';
 import { getProvidersByIds } from '../../utils';
+import { categoryState } from '../../recoil/atom';
+import { ActionIcons } from '../common';
 
-const AccordionLabel = ({ title, providers: providerIds }) => {
+const AccordionLabel = ({ title, id, type, providers: providerIds }) => {
+  const category = useRecoilValue(categoryState);
   const providers = getProvidersByIds(providerIds);
 
   return (
     <Group noWrap>
       <Badges providers={providers} spacing={30} />
       <div>
-        <Text>{title}</Text>
-        <CollectionButtons />
+        <Text fz="lg" fw={700}>
+          {title}
+        </Text>
+        <Box mt="xs">
+          <ActionIcons size={16} id={id} type={type} category={category} />
+        </Box>
       </div>
     </Group>
   );
