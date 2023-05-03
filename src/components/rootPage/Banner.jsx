096cc79fbc4b1bbb23396374ead3001c4db8cd46
5kdk <tylerk1121@gmail.com>
2023-05-04 00:51:13 +0900

    Fix: Public 폴더 이미지에서 src내 이미지로 변경

--- src/components/rootPage/Banner.jsx
+++ src/components/rootPage/Banner.jsx
@@ -1,56 +1,41 @@
 import { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
 import { Carousel } from '@mantine/carousel';
-import { rem } from '@mantine/core';
+import { Image } from '@mantine/core';
 import styled from '@emotion/styled';
 
-const data = [
-  '/assets/images/temp.jpg',
-  '/assets/images/temp.jpg',
-  '/assets/images/temp.jpg',
-  '/assets/images/temp.jpg',
-  '/assets/images/temp.jpg',
-];
+const BillBoard = styled(Carousel)`
+  & .mantine-Carousel-indicator {
+    width: 0.9375rem;
+    height: 0.125rem;
+    transition: 'width 250ms ease';
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background-image: url(${({ url }) => url});
-  background-size: cover;
+    &[data-active] {
+      width: 6.25rem;
+    }
+  }
 `;
 
-const Card = ({ url }) => <Container url={url}></Container>;
+const data = ['/assets/images/temp.jpg', '/assets/images/temp.jpg', '/assets/images/temp.jpg'];
+
+const SubContainer = () => <Image height={400} src={'../../assets/images/temp.jpg'} fit="cover" />;
 
 const Banner = () => {
   const autoplay = useRef(Autoplay({ delay: 5000 }));
   return (
-    <Carousel
-      maw={'100%'}
-      mx="auto"
-      mb={'md'}
+    <BillBoard
       withIndicators
-      height={rem(400)}
+      height={400}
       plugins={[autoplay.current]}
       loop
-      styles={{
-        indicator: {
-          width: rem(12),
-          height: rem(4),
-          transition: 'width 250ms ease',
-
-          '&[data-active]': {
-            width: rem(40),
-          },
-        },
-      }}
       onMouseEnter={autoplay.current.stop}
       onMouseLeave={autoplay.current.reset}>
       {data.map((item, idx) => (
         <Carousel.Slide key={item + idx}>
-          <Card url={item} />
+          <SubContainer url={item} />
         </Carousel.Slide>
       ))}
-    </Carousel>
+    </BillBoard>
   );
 };
 
