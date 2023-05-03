0df6ec306e7c773452cbc6ebbcfe29794fb85887
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-03 16:08:28 +0900

    Refactor: ActionIcon 위치 이동
    - 아코디언 열렸을때 아이콘 보이도록 이동

--- src/components/myPage/AccordionLabel.jsx
+++ src/components/myPage/AccordionLabel.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import { Box, Group, Text } from '@mantine/core';
-import { useRecoilValue } from 'recoil';
+import { Group, Text } from '@mantine/core';
 import { Badges } from '..';
 import { getProvidersByIds } from '../../utils';
-import { categoryState } from '../../recoil/atom';
-import { ActionIcons } from '../common';
 
-const AccordionLabel = ({ title, id, type, providers: providerIds }) => {
-  const category = useRecoilValue(categoryState);
+const AccordionLabel = ({ title, providers: providerIds }) => {
   const providers = getProvidersByIds(providerIds);
 
   return (
     <Group noWrap>
       <Badges providers={providers} spacing={30} />
-      <div>
-        <Text fz="lg" fw={700}>
-          {title}
-        </Text>
-        <Box mt="xs">
-          <ActionIcons size={16} id={id} type={type} category={category} />
-        </Box>
-      </div>
+      <Text fz="lg" fw={700}>
+        {title}
+      </Text>
     </Group>
   );
 };
