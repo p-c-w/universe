a89696751d3fbbf6fea1bbdc60ac608f049701c9
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-04 13:01:51 +0900

    Comment: 불필요한 주석 제거

--- src/components/common/ModalSkeleton.jsx
+++ src/components/common/ModalSkeleton.jsx
@@ -1,14 +1,3 @@
-// import React from 'react';
-// import { Modal } from '@mantine/core';
-
-// const ModalSkeleton = () => (
-//   <Modal.Root opened={true} size={1000} centered zIndex="9999">
-//     <Modal.Overlay />
-//   </Modal.Root>
-// );
-
-// export default ModalSkeleton;
-
 import React from 'react';
 import { Skeleton, Modal, Overlay } from '@mantine/core';
 
