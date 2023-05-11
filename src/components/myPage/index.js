a039292764e7711cb5f1c696b8b217a737430ebd
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-11 15:07:22 +0900

    Style: index 파일 순서 정리
    - Mypage index 파일내 export하는 파일들을 컴포넌트별로 구분 및 순서 정리

--- src/components/myPage/index.js
+++ src/components/myPage/index.js
@@ -1,17 +1,22 @@
 export { default as MypageTitle } from './MypageTitle';
-export { default as CollectionButton } from './CollectionButton';
-export { default as Collection } from './Collection';
-export { default as Collections } from './Collections';
-export { default as ProviderBadges } from './ProviderBadges';
-export { default as Statistics } from './Statistics';
-export { default as SubscriptionProviders } from './SubscriptionProviders';
-export { default as CurrentSubscriptionInfo } from './CurrentSubscriptionInfo';
-export { default as SubscriptionEditor } from './SubscriptionEditor';
+
 export { default as PredictedFeeWrapper } from './PredictedFeeWrapper';
 export { default as PredictedSubscription } from './PredictedSubscription';
-export { default as AccordionLabel } from './AccordionLabel';
+export { default as CurrentSubscriptionInfo } from './CurrentSubscriptionInfo';
+export { default as SubscriptionEditor } from './SubscriptionEditor';
+export { default as SubscriptionProviders } from './SubscriptionProviders';
+export { default as CurrentUnsubscriptionInfo } from './CurrentUnsubscriptionInfo';
+
+export { default as Statistics } from './Statistics';
 export { default as StatisticByProvider } from './StatisticByProvider';
+
+export { default as CollectionCategoryButton } from './CollectionCategoryButton';
+export { default as Collections } from './Collections';
+export { default as Collection } from './Collection';
 export { default as CollectionItem } from './CollectionItem';
+export { default as AccordionLabel } from './AccordionLabel';
+export { default as EmptyCollection } from './EmptyCollection';
+export { default as ProviderBadges } from './ProviderBadges';
+
 export { default as CollectionSkeleton } from './CollectionSkeleton';
 export { default as BoxSkeleton } from './BoxSkeleton';
-export { default as EmptyCollection } from './EmptyCollection';
