f890522a45813b80debef70e1c193b3416867e0f
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-11 11:41:50 +0900

    Refactor: 컴포넌트 분리
    - 현재 구독 정보 컴포넌트에서 '구독중이지만 보고있지 않아요' 영역을 나타내는 컴포넌트 분리
    - CurrentSubscriptionInfo에서 CurrentUnsubscriptionInfo를 분리

---
+++ src/components/myPage/CurrentUnsubscriptionInfo.jsx
@@ -0,0 +1,55 @@
+import { Box, Title } from '@mantine/core';
+import { ProviderBadges } from './index';
+import { getProvidersIdsByList, getProvidersByIds } from '../../utils';
+import { PROVIDERS } from '../../constants';
+import { useProviderQueries } from '../../hooks/queries';
+
+const CurrentUnsubscriptionInfo = ({ subscribeList, watchList }) => {
+  const queries = useProviderQueries(watchList, {
+    select: data => ({
+      id: data.id,
+      providers: data.results.KR?.flatrate
+        ?.map(provider => provider.provider_id)
+        ?.filter(id => Object.prototype.hasOwnProperty.call(PROVIDERS, id)),
+    }),
+    enabled: !!watchList.length,
+  });
+
+  const subscribeProviderIds = getProvidersIdsByList(subscribeList);
+
+  const whatchProvidersWithContenId = [];
+  queries.forEach(query => {
+    if (query.isSuccess) whatchProvidersWithContenId.push(query.data);
+  });
+
+  const whatchProviderIds = whatchProvidersWithContenId?.flatMap(content => content.providers);
+
+  const unWatchedProviderIds = subscribeProviderIds?.filter(
+    subscribeProviderId => !whatchProviderIds.includes(subscribeProviderId)
+  );
+
+  const unWatchedProvidersInfoList = getProvidersByIds(unWatchedProviderIds);
+
+  return (
+    <Box mt={16}>
+      {!subscribeProviderIds.length ? (
+        <Title order={5} mb={10}>
+          현재 구독 중인 서비스가 없어요
+        </Title>
+      ) : unWatchedProvidersInfoList?.length ? (
+        <>
+          <Title order={5} mb={10}>
+            구독하고 있지만 보고 있지 않아요
+          </Title>
+          <ProviderBadges providers={unWatchedProvidersInfoList} />
+        </>
+      ) : (
+        <Title order={5} mb={10}>
+          구독중인 모든 서비스를 사용하고 있어요
+        </Title>
+      )}
+    </Box>
+  );
+};
+
+export default CurrentUnsubscriptionInfo;
