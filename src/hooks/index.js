4a7158388a7560fc115e17b1727ed2b16afeeaf0
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-12 17:34:19 +0900

    Style: index 파일 가독성 확보

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,6 @@
-export { default as useStatsByProvider } from './useStatsByProvider';
 export { default as useColorScheme } from './useColorScheme';
 export { default as useObserver } from './useObserver';
 export { default as usePagination } from './usePagination';
+
+export { default as useStatsByProvider } from './useStatsByProvider';
+export { default as useStatsByMonthly } from './useStatsByMonthly';
--- src/recoil/atom/index.js
+++ src/recoil/atom/index.js
@@ -1,5 +1,7 @@
-export { default as sideNavOpenedState } from './sideNavOpenedState';
 export { default as userState } from './userState';
+
 export { default as categoryState } from './categoryState';
+export { default as sideNavOpenedState } from './sideNavOpenedState';
+
 export { default as statsByProviderState } from './statsByProviderState';
-export { default as statisticByMonthlyState } from './statsByMonthlyState';
+export { default as statsByMonthlyState } from './statsByMonthlyState';
