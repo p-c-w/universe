23076c7d8ee024e8066237529dce74511766fd71
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-12 17:32:25 +0900

    Feat: apecchart line 그래프 컴포넌트 및 옵션 추가

--- src/components/myPage/StatsByMonthly.jsx
+++ src/components/myPage/StatsByMonthly.jsx
@@ -1,13 +1,40 @@
+import ReactApexChart from 'react-apexcharts';
 import { Paper, Title, Flex } from '@mantine/core';
 
-const StatsByMonthly = () => (
-  <Paper withBorder p={40} pt="sm" radius="md" h="100%">
-    <Flex direction="column" justify="center" h="100%">
-      <Title order={3} align="left">
-        Universe 분석
-      </Title>
-    </Flex>
-  </Paper>
-);
+const StatsByMonthly = () => {
+  const chartData = {
+    options: {
+      chart: {
+        id: 'basic-line',
+        toolbar: {
+          show: false,
+        },
+      },
+      grid: {
+        borderColor: 'transparent',
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+    },
+    series: [
+      {
+        name: 'series-1',
+      },
+    ],
+  };
+
+  return (
+    <Paper withBorder p={40} pt="sm" radius="md" h="100%">
+      <Flex direction="column" justify="center" h="100%">
+        <Title order={3} align="left">
+          Universe 분석
+        </Title>
+
+        <ReactApexChart options={chartData.options} series={chartData.series} type="line" height={250} />
+      </Flex>
+    </Paper>
+  );
+};
 
 export default StatsByMonthly;
