e58d13a3eeeea677f3f09104b5957acf3fc4b72a
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-12 17:21:30 +0900

    Feat: 월별 통계 atom 초기값 설정

--- src/recoil/atom/statsByMonthlyState.js
+++ src/recoil/atom/statsByMonthlyState.js
@@ -1,8 +1,10 @@
 import { atom } from 'recoil';
 
+const defaultData = Array.from({ length: 12 }, () => 0);
+
 const statsByMonthlyState = atom({
   key: 'statsByMonthlyState',
-  default: [],
+  default: defaultData,
 });
 
 export default statsByMonthlyState;
