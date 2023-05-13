e618582fa625d7633fe93a1e426d2f6b61e5fb68
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-14 00:12:43 +0900

    Remove: 불필요한 useStats 커스텀 훅 삭제

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,7 +2,6 @@ export { default as useColorScheme } from './useColorScheme';
 export { default as useObserver } from './useObserver';
 export { default as usePagination } from './usePagination';
 
-export { default as useStats } from './useStats';
 export { default as useStatsByProvider } from './useStatsByProvider';
 export { default as useStatsByMonthly } from './useStatsByMonthly';
 export { default as useStatsByGenre } from './useStatsByGenre';
