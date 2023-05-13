eb4889760c83cf50cc07627c2202a0946b4ace8a
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-14 00:11:51 +0900

    Remove: 불필요한 recoil atom 삭제

--- src/recoil/atom/index.js
+++ src/recoil/atom/index.js
@@ -2,7 +2,3 @@ export { default as userState } from './userState';
 
 export { default as categoryState } from './categoryState';
 export { default as sideNavOpenedState } from './sideNavOpenedState';
-
-export { default as statsByProviderState } from './statsByProviderState';
-export { default as statsByMonthlyState } from './statsByMonthlyState';
-export { default as statsByGenreState } from './statsByGenreState';
