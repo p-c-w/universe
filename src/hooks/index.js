1d5b4dbfdee16dd708450d035e28e8e0f1516c76
5kdk <tylerk1121@gmail.com>
2023-05-13 15:32:04 +0900

    Feat: 로그아웃용 커스텀훅 index 생성

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 export { default as useColorScheme } from './useColorScheme';
 export { default as useObserver } from './useObserver';
 export { default as usePagination } from './usePagination';
+export { default as useSignout } from './useSignout';
 
 export { default as useStatsByProvider } from './useStatsByProvider';
 export { default as useStatsByMonthly } from './useStatsByMonthly';
