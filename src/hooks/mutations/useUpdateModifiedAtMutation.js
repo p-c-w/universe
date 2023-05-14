001722a29ec825f8bd456c962ca37e3bcea54b9e
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-14 21:39:39 +0900

    Feat: 컨텐츠 추가일 수정하는 커스텀 mutation 구현

--- src/hooks/mutations/useUpdateModifiedAtMutation.js
+++ src/hooks/mutations/useUpdateModifiedAtMutation.js
@@ -1,13 +1,15 @@
 import { useRecoilValue } from 'recoil';
 import { USER_INFO_QUERY_KEY } from '../../constants';
-import { updateSubscribeList } from '../../api';
+import { useUpdateModifiedAt } from '../../api';
 import { useGenericMutation } from '.';
 import { userState } from '../../recoil/atom';
 
 /**
  * useUpdateModifiedAtMutation - 사용자의 history list item 추가 날짜를 업데이트하는데 사용되는 커스텀 훅입니다.
  *
- * @param {string} name - 변경할 사용자 이름
+ * @param {string} list - userInfo 객체에서 수정할 배열의 이름
+ * @param {number} id - 수정할 content의 id
+ * @param {string} value - 수정할 값
  */
 
 const useUpdateModifiedAtMutation = () => {
@@ -15,9 +17,12 @@ const useUpdateModifiedAtMutation = () => {
 
   return useGenericMutation({
     queryKey: [USER_INFO_QUERY_KEY, email],
-    mutationFn: updateSubscribeList,
-    onMutate({ newList }) {
-      return userInfo => ({ ...userInfo, subscribe_list: newList });
+    mutationFn: useUpdateModifiedAt,
+    onMutate({ list, id, value }) {
+      return userInfo => ({
+        ...userInfo,
+        [list]: userInfo[list].map(item => (item.id === id ? { ...item, modified_at: value } : item)),
+      });
     },
   });
 };
