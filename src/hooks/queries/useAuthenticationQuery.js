bdb2a3b21fd69bc9ffc314f02fa25b8c7c96284b
5kdk <tylerk1121@gmail.com>
2023-05-16 03:37:46 +0900

    Fix: Login 남발되는 에러 수정

--- src/hooks/queries/useAuthenticationQuery.js
+++ src/hooks/queries/useAuthenticationQuery.js
@@ -1,15 +1,38 @@
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
 import { IS_AUTHENTICATED_QUERY_KEY } from '../../constants';
+import { isLoginState, userState } from '../../recoil/atom';
 
 const useAuthenticationQuery = () => {
-  const { isSuccess, isLoading, isFetched, error, data } = useQuery({
+  const setUserId = useSetRecoilState(userState);
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState);
+
+  const { isSuccess, isFetched, error, data } = useQuery({
     queryKey: [IS_AUTHENTICATED_QUERY_KEY],
-    queryFn: () => axios('/api/auth/verify'),
+    queryFn: async () => {
+      const res = await axios('/api/auth/verify');
+
+      return res.data;
+    },
+    staleTime: 1000,
     retry: false,
   });
 
-  return { isSuccess, isLoading, isFetched, error, data };
+  useEffect(() => {
+    if (error) {
+      setUserId(null);
+      setIsLogin(data.isLogin);
+    }
+
+    if (isSuccess && data.email) {
+      setUserId(data.email);
+      setIsLogin(data.isLogin);
+    }
+  }, [data, error, isSuccess, setIsLogin, setUserId]);
+
+  return { isSuccess, isFetched, error, data, isLogin };
 };
 
 export default useAuthenticationQuery;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,23 +1,14 @@
-import { useRecoilState } from 'recoil';
-import { useEffect } from 'react';
 import { GlobalShell } from '../components/common';
 import { Banner, Board } from '../components/rootPage';
 import { useAuthenticationQuery } from '../hooks/queries';
-import { userState } from '../recoil/atom';
 
 const Root = () => {
-  const { isSuccess, data } = useAuthenticationQuery();
-  const [user, setUser] = useRecoilState(userState);
-
-  useEffect(() => {
-    if (isSuccess) setUser(data.data);
-    else setUser(null);
-  }, [data, isSuccess, setUser]);
+  const { isLogin } = useAuthenticationQuery();
 
   return (
     <>
       <GlobalShell>
-        {!user && <Banner />}
+        {!isLogin && <Banner />}
         <Board />
       </GlobalShell>
     </>
