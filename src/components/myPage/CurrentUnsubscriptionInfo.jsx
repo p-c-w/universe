02efb0c30c2cb783ef5616ba950926e86c2379ef
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-15 19:31:36 +0900

    Design: 마이페이지 구독하고 있지만 보고 있지 않아요 영역 반응형 스타일 적용

--- src/components/myPage/CurrentUnsubscriptionInfo.jsx
+++ src/components/myPage/CurrentUnsubscriptionInfo.jsx
@@ -1,10 +1,14 @@
 import { Box, Title } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { ProviderBadges } from './index';
 import { getProvidersIdsByList, getProvidersByIds } from '../../utils';
 import { PROVIDERS } from '../../constants';
 import { useProviderQueries } from '../../hooks/queries';
 
 const CurrentUnsubscriptionInfo = ({ subscribeList, watchList }) => {
+  const smallScreen = useMediaQuery('(max-width: 48rem)');
+  const xsmallScreen = useMediaQuery('(max-width: 30rem)');
+
   const queries = useProviderQueries(watchList, {
     select: data => ({
       id: data.id,
@@ -25,18 +29,21 @@ const CurrentUnsubscriptionInfo = ({ subscribeList, watchList }) => {
   return (
     <Box mt={16}>
       {!subscribeProviderIds.length ? (
-        <Title order={5} mb={10}>
+        <Title order={5} mb={10} fz={xsmallScreen ? 12 : 16}>
           현재 구독 중인 서비스가 없어요
         </Title>
       ) : unWatchedProvidersInfoList?.length ? (
         <>
-          <Title order={5} mb={10}>
+          <Title order={5} mb={10} fz={xsmallScreen ? 12 : 16}>
             구독하고 있지만 보고 있지 않아요
           </Title>
-          <ProviderBadges providers={unWatchedProvidersInfoList} />
+          <ProviderBadges
+            providers={unWatchedProvidersInfoList}
+            size={xsmallScreen ? 'sm' : smallScreen ? 'md' : 'lg'}
+          />
         </>
       ) : (
-        <Title order={5} mb={10}>
+        <Title order={5} mb={10} fz={xsmallScreen ? 12 : 16}>
           구독중인 모든 서비스를 사용하고 있어요
         </Title>
       )}
