e41ebfce67819df85e79872fad083e7792897fca
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-15 17:59:39 +0900

    Design: 크기 변경

--- src/components/myPage/Collection.jsx
+++ src/components/myPage/Collection.jsx
@@ -1,26 +1,13 @@
 import { useRef, Suspense, useState, useEffect } from 'react';
 import { Accordion } from '@mantine/core';
-import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+import { useDisclosure } from '@mantine/hooks';
 import { useRecoilValue } from 'recoil';
-import styled from '@emotion/styled';
 import { useCollectionQueries } from '../../hooks/queries';
 import { DetailModalWrapper, ModalSkeleton } from '../common';
 import { CollectionItem, ConfirmModal } from '.';
 import { categoryState } from '../../recoil/atom';
 
-const ItemContainer = styled(Accordion)`
-  .mantine-Accordion-label {
-    padding-top: ${props => props.space};
-    padding-bottom: ${props => props.space};
-  }
-  .mantine-Accordion-chevron {
-    margin-left: ${props => props.space};
-  }
-`;
-
 const Collection = ({ collection, setItemSelected, setImgSrc }) => {
-  const xsmallScreen = useMediaQuery('(max-width: 30rem)');
-
   const category = useRecoilValue(categoryState);
   const collectionQueries = useCollectionQueries(collection);
   const [detailModalOpened, { open: openDetailModal, close: closeDetailModal }] = useDisclosure(false);
@@ -53,12 +40,7 @@ const Collection = ({ collection, setItemSelected, setImgSrc }) => {
 
   return (
     <>
-      <ItemContainer
-        variant="separated"
-        w="100%"
-        onChange={handleChange}
-        value={value}
-        space={xsmallScreen && '0.5rem'}>
+      <Accordion variant="separated" w="100%" onChange={handleChange} value={value}>
         {allQueriesSucceeded &&
           collectionList?.map(item => (
             <CollectionItem
@@ -69,7 +51,7 @@ const Collection = ({ collection, setItemSelected, setImgSrc }) => {
               openComfirmModal={openComfirmModal}
             />
           ))}
-      </ItemContainer>
+      </Accordion>
       {detailModalOpened && (
         <Suspense fallback={<ModalSkeleton />}>
           <DetailModalWrapper opened={detailModalOpened} close={closeDetailModal} id={clicked.id} type={clicked.type} />
