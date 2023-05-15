78e19f1f467b8d63add2144c70938c298507a9f6
5kdk <tylerk1121@gmail.com>
2023-05-16 00:43:40 +0900

    Feat: 사이드 네비게이션 듀레이션 상수 index 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,8 @@ export { default as colors } from './colors';
 export { default as PRICE } from './priceInfo';
 export { default as COLLECTION_BUTTON } from './collectionButtons';
 export { default as PAGE_LIMIT } from './pageLimit';
+export { default as SIDE_NAV_DURATION } from './sideNavDuration';
+
 export { userInfoQueryKey as USER_INFO_QUERY_KEY } from './userQueryKeys';
 export { userContentQueryKey as USER_CONTENT_QUERY_KEY } from './userQueryKeys';
 export { isAuthenticatedQueryKey as IS_AUTHENTICATED_QUERY_KEY } from './userQueryKeys';
