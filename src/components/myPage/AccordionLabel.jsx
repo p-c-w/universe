01dec57eece1385046666544d156f1a5bd5663da
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-15 16:09:45 +0900

    Design: 컬렉션 아이템 제목에 useMediaQuery 적용

--- src/components/myPage/AccordionLabel.jsx
+++ src/components/myPage/AccordionLabel.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Group, Text } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { Badges, Badge } from '../common';
 import { getProvidersByIds } from '../../utils';
 
 const AccordionLabel = ({ title, providers: providerIds }) => {
   const providers = getProvidersByIds(providerIds);
 
+  const smallScreen = useMediaQuery('(max-width: 48rem)');
+  const xsmallScreen = useMediaQuery('(max-width: 30rem)');
+
   return (
     <Group noWrap>
       {providers ? (
         <Badges providers={providers} spacing={25} />
       ) : (
-        <Badge src={`./assets/logos/universeLogoWhite.svg`} size="2.2rem" />
+        <Badge src={`./assets/logos/universeLogoWhite.svg`} size={35} />
       )}
-      <Text fz="lg" fw={700}>
+      <Text fz={xsmallScreen ? 'sm' : smallScreen ? 'md' : 'lg'} fw={700}>
         {title}
       </Text>
     </Group>
