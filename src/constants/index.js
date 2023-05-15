51fec36d3b43da0a85aacab3ac72a6bef76b57c4
5kdk <tylerk1121@gmail.com>
2023-05-16 03:38:40 +0900

    Feat: goToTop 구현을 위한 버튼 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,8 @@ export { default as PRICE } from './priceInfo';
 export { default as COLLECTION_BUTTON } from './collectionButtons';
 export { default as PAGE_LIMIT } from './pageLimit';
 export { default as SIDE_NAV_DURATION } from './sideNavDuration';
+export { default as BUTTON_START_Y } from './buttonStartY';
+export { default as SCROLL_DELAY } from './scrollDelay';
 
 export { userInfoQueryKey as USER_INFO_QUERY_KEY } from './userQueryKeys';
 export { userContentQueryKey as USER_CONTENT_QUERY_KEY } from './userQueryKeys';
