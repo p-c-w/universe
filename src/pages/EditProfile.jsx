02b9a137c8a07accfd85e617fba738361da109cd
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-16 19:29:23 +0900

    Feat: 비밀번호 편집 모달로 수정

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container } from '@mantine/core';
 import { GlobalShell } from '../components/common';
+import { EditprofileTitle, EditPassword } from '../components/editProfile';
 
 const EditProfile = () => {
   return (
     <GlobalShell>
       <Container mt="1rem" mx="auto" size={'100%'} w={1240} miw={375}>
-        Hi
+        <EditprofileTitle />
+        <EditPassword />
       </Container>
     </GlobalShell>
   );
