9274e83a523e436905397fe456afd92b86b637fd
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-16 19:22:21 +0900

    Feat: 프로필 편집 index 생성

---
+++ src/components/editProfile/index.js
@@ -0,0 +1,5 @@
+export { default as EditprofileTitle } from './EditprofileTitle';
+export { default as EditPassword } from './EditPassword';
+export { default as EditAvatar } from './EditAvatar';
+export { default as DeleteUser } from './DeleteUser';
+export { default as Password } from './Password';
