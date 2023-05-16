ffb3a7ad87cfa567080dc2a3784803bffd24aec4
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-17 01:32:16 +0900

    Feat: 회원탈퇴 서버 연결

--- src/components/editProfile/DeleteUser.jsx
+++ src/components/editProfile/DeleteUser.jsx
@@ -2,12 +2,46 @@ import React, { useState } from 'react';
 import { Title, Container, Flex, Button, Modal, Text, Checkbox } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { useUserQuery } from '../../hooks/queries';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../recoil/atom';
+import { notifications } from '@mantine/notifications';
+import { IconX, IconCheck } from '@tabler/icons-react';
 
 const DeleteUser = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [checked, setChecked] = useState(true);
+  const email = useRecoilValue(userState);
 
   const { userInfo: name } = useUserQuery({ select: userInfo => userInfo.name });
+
+  const handleClick = async () => {
+    console.log('Hi!');
+    try {
+      const { data: alarm } = await axios.delete(`/api/auth/withdrawal/${email}`);
+
+      notifications.show({
+        withCloseButton: true,
+        autoClose: 3000,
+        title: 'Withdrawal Success',
+        message: alarm,
+        color: 'green',
+        icon: <IconCheck />,
+        loading: false,
+      });
+      close();
+    } catch (error) {
+      notifications.show({
+        withCloseButton: true,
+        autoClose: 3000,
+        title: 'Withdrawal Failure',
+        message: '알 수 없는 오류가 발생했습니다.',
+        color: 'red',
+        icon: <IconX />,
+        loading: false,
+      });
+    }
+  };
 
   return (
     <Container>
@@ -19,7 +53,7 @@ const DeleteUser = () => {
             <Button fullWidth variant="outline" onClick={close}>
               취소하기
             </Button>
-            <Button fullWidth variant="outline" disabled={checked}>
+            <Button onClick={handleClick} fullWidth variant="outline" disabled={checked}>
               탈퇴하기
             </Button>
           </Flex>
@@ -27,7 +61,9 @@ const DeleteUser = () => {
       </Modal>
       <Flex gap={50}>
         <Title order={5}>회원탈퇴</Title>
-        <Button onClick={open} variant="outline"></Button>
+        <Button onClick={open} variant="outline">
+          탈퇴
+        </Button>
       </Flex>
     </Container>
   );
