7a72fa0b41fc2f3ec38614735ae4f66732e7e04e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-17 03:16:21 +0900

    Refactor: 불필요한 useState 제거 및 스키마 변경으로 handleSubmit 사용

--- src/components/editProfile/EditPassword.jsx
+++ src/components/editProfile/EditPassword.jsx
@@ -5,7 +5,7 @@ import { Password } from '.';
 import { useDisclosure } from '@mantine/hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { signUpSchema } from '../../schema/schema';
+import { ChangePwSchema } from '../../schema/schema';
 import { notifications } from '@mantine/notifications';
 import { IconX, IconCheck } from '@tabler/icons-react';
 import { useRecoilValue } from 'recoil';
@@ -14,18 +14,14 @@ import { userState } from '../../recoil/atom';
 const EditPassword = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const email = useRecoilValue(userState);
-  const [nowPw, setNowPw] = useState(null);
-  const [newPw, setNewPw] = useState(null);
-  const [newConfirmPw, setNewConfirmPw] = useState(null);
 
-  const { control, trigger } = useForm({
-    resolver: zodResolver(signUpSchema),
+  const { control, trigger, handleSubmit } = useForm({
+    resolver: zodResolver(ChangePwSchema),
   });
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+  const onSubmit = async data => {
     try {
-      const { data: alarm } = await axios.patch('/api/auth/changepw', { email, nowPw, newPw, newConfirmPw });
+      const { data: alarm } = await axios.patch('/api/auth/changepw', { email, ...data });
 
       notifications.show({
         withCloseButton: true,
@@ -65,17 +61,15 @@ const EditPassword = () => {
   return (
     <Container my={10} p={0}>
       <Modal opened={opened} centered onClose={close} title="비밀번호 변경" style={{ marginTop: '10rem' }}>
-        <form onSubmit={handleSubmit}>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleSubmit(onSubmit)();
+          }}>
           <Flex direction="column" gap={10}>
-            <Password label="현재 비밀번호" control={control} trigger={trigger} setPw={setNowPw} />
-            <Password label="새 비밀번호" control={control} trigger={trigger} name="password" setPw={setNewPw} />
-            <Password
-              label="새 비밀번호 확인"
-              control={control}
-              trigger={trigger}
-              name="confirmPassword"
-              setPw={setNewConfirmPw}
-            />
+            <Password label="현재 비밀번호" control={control} trigger={trigger} name="nowPassword" />
+            <Password label="새 비밀번호" control={control} trigger={trigger} name="newPassword" />
+            <Password label="새 비밀번호 확인" control={control} trigger={trigger} name="confirmPassword" />
             <Button type="submit">제출</Button>
           </Flex>
         </form>
