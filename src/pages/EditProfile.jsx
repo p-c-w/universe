9f11468a9bf55178b8b9bcf22e46ec24bfbf2c11
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-16 17:58:00 +0900

    Feat: 프로필 편집 페이지 컴포넌트 초기 셋팅

---
+++ src/pages/EditProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '@mantine/core';
+import { GlobalShell } from '../components/common';
+
+const EditProfile = () => {
+  return (
+    <GlobalShell>
+      <Container mt="1rem" mx="auto" size={'100%'} w={1240} miw={375}>
+        Hi
+      </Container>
+    </GlobalShell>
+  );
+};
+
+export default EditProfile;
