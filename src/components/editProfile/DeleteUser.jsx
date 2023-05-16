0853f1624760b9f11f5f45a868a420da7973533e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-17 00:41:34 +0900

    Feat: userName 추가

--- src/components/editProfile/DeleteUser.jsx
+++ src/components/editProfile/DeleteUser.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import { Title, Container, Flex, Button, Modal, Text, Checkbox } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useUserQuery } from '../../hooks/queries';
 
 const DeleteUser = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [checked, setChecked] = useState(true);
 
+  const { userInfo: name } = useUserQuery({ select: userInfo => userInfo.name });
+
   return (
     <Container>
       <Modal opened={opened} centered onClose={close} title="회원 탈퇴" style={{ marginTop: '10rem' }}>
         <Flex direction={'column'} gap={20}>
-          <Text> 계정 삭제시 회원님의 유니버스가 삭제되며 복구 불가능합니다.</Text>
+          <Text> 계정 삭제시 {name}님의 유니버스가 삭제되며 복구 불가능합니다.</Text>
           <Checkbox onChange={event => setChecked(!event.currentTarget.checked)} label="이에 동의하십니까?" />
           <Flex gap={3}>
             <Button fullWidth variant="outline">
