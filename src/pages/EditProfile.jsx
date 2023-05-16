06633b115f5c2485824d5038e03ad93d045b658e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-17 02:40:03 +0900

    Refactor: 컴포넌트 병합

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Container } from '@mantine/core';
+import { Container, Flex, Title, Text } from '@mantine/core';
 import { GlobalShell } from '../components/common';
-import { EditprofileTitle, EditPassword } from '../components/editProfile';
+import { EditPassword, DeleteUser } from '../components/editProfile';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../recoil/atom';
 
 const EditProfile = () => {
+  const email = useRecoilValue(userState);
+
   return (
     <GlobalShell>
-      <Container mt="1rem" mx="auto" size={'100%'} w={1240} miw={375}>
-        <EditprofileTitle />
-        <EditPassword />
-      </Container>
+      <Flex w={1240} mt="1rem" mx="auto" size={'100%'} miw={375} direction={'column'} justify={'center'}>
+        <Container w={400} mt={100}>
+          <Title order={2} align="center" my={20}>
+            👾 나의 프로필 수정 👾
+          </Title>
+          <Flex gap={50} mt={10} justify={'space-between'}>
+            <Title order={5}>이메일</Title>
+            <Text>{email}</Text>
+          </Flex>
+          <EditPassword />
+          <DeleteUser />
+        </Container>
+      </Flex>
     </GlobalShell>
   );
 };
