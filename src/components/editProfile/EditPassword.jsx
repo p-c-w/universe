3e1af14834630b96034057933ddec6ce96b42a17
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-17 20:18:55 +0900

    Remove: 불필요한 주석 제거

--- src/components/editProfile/EditPassword.jsx
+++ src/components/editProfile/EditPassword.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import React from 'react';
-import { Container, Title, Flex, Button, Modal } from '@mantine/core';
+import { Container, Title, Flex, Button } from '@mantine/core';
 import { Password } from '.';
-// import { useDisclosure } from '@mantine/hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { ChangePwSchema } from '../../schema/schema';
@@ -13,7 +12,6 @@ import { userState } from '../../recoil/atom';
 import { modals } from '@mantine/modals';
 
 const EditPassword = () => {
-  // const [opened, { open, close }] = useDisclosure(false);
   const email = useRecoilValue(userState);
 
   const { control, trigger, handleSubmit } = useForm({
@@ -82,20 +80,6 @@ const EditPassword = () => {
 
   return (
     <Container my={10} p={0}>
-      {/* <Modal opened={opened} centered onClose={close} title="비밀번호 변경" style={{ marginTop: '10rem' }}>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            handleSubmit(onSubmit)();
-          }}>
-          <Flex direction="column" gap={10}>
-            <Password label="현재 비밀번호" control={control} trigger={trigger} name="nowPassword" />
-            <Password label="새 비밀번호" control={control} trigger={trigger} name="newPassword" />
-            <Password label="새 비밀번호 확인" control={control} trigger={trigger} name="confirmPassword" />
-            <Button type="submit">제출</Button>
-          </Flex>
-        </form>
-      </Modal> */}
       <Flex gap={50} align={'center'} justify={'space-between'}>
         <Title order={5}>비밀번호</Title>
         <Button onClick={openModal} variant="outline">
