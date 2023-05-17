8695095cabf408c15868da9b088e4661f11e845f
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-18 01:53:59 +0900

    Remove: 불필요한 코드 제거

--- src/components/myPage/CollectionItem.jsx
+++ src/components/myPage/CollectionItem.jsx
@@ -6,18 +6,24 @@ import { modals } from '@mantine/modals';
 import { useMediaQuery } from '@mantine/hooks';
 import { IconLayersLinked, IconTrash } from '@tabler/icons-react';
 import { AccordionLabel, ModifiedDate, ConfirmModal } from '.';
-import { ActionIcons } from '../common';
+import { ActionIcons, DetailModal } from '../common';
 
-const CollectionItem = ({ item, setClicked, openDetailModal, setSelectedItem, setIsItemSelected }) => {
+const CollectionItem = ({ item, setSelectedItem, setIsItemSelected }) => {
   const listName = useRecoilValue(categoryState);
   const [hovered, setHovered] = useState(false);
 
   const smallScreen = useMediaQuery('(max-width: 30rem)');
   const xsmallScreen = useMediaQuery('(max-width: 30rem)');
 
-  const handleDetailClick = item => {
-    setClicked(item);
-    openDetailModal();
+  const DetailClick = () => {
+    modals.open({
+      centered: true,
+      withCloseButton: false,
+      size: 950,
+      padding: 0,
+      m: 0,
+      children: <DetailModal type={item.type} id={item.id} />,
+    });
   };
 
   const handleMouseEnter = () => setHovered(true);
@@ -28,8 +34,8 @@ const CollectionItem = ({ item, setClicked, openDetailModal, setSelectedItem, se
     e.stopPropagation();
 
     modals.open({
-      centered: true,
       title: ' 해당 컨텐츠를 삭제하시겠습니까?',
+      centered: true,
       children: <ConfirmModal id={item.id} listName={listName} />,
     });
   };
@@ -64,14 +70,7 @@ const CollectionItem = ({ item, setClicked, openDetailModal, setSelectedItem, se
             <ModifiedDate id={item?.id} date={item?.modified_at} />
             <div>
               <Tooltip label="더보기" position="bottom-end" withArrow withinPortal>
-                <Button
-                  m={0}
-                  pl={0}
-                  pb={3}
-                  variant="transparent"
-                  onClick={() => handleDetailClick({ id: item?.id, type: item?.type })}
-                  fz={12}
-                  aria-label="more">
+                <Button m={0} pl={0} pb={3} variant="transparent" onClick={DetailClick} fz={12} aria-label="more">
                   <Text fz={xsmallScreen ? 'xs' : 'sm'}>상세페이지로</Text>
                   <ThemeIcon variant="transparent">
                     <IconLayersLinked size={16} />
