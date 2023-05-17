e667fc81caee7bc30a4f927b9f7e20a377bbb256
5kdk <tylerk1121@gmail.com>
2023-05-17 23:18:18 +0900

    Feat: goToTop 유틸함수 생성

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,3 +9,5 @@ export { default as getNewSubscribeList } from './getNewSubscribeList';
 export { default as generateInitial } from './generateInitial';
 
 export { default as formatDate } from './formatDate';
+
+export { default as goToTop } from './goToTop';
