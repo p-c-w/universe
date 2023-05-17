6c5ed2e3e5e306c95b18212fb5e71964efc1340a
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-18 01:52:49 +0900

    Refactor: modals Manager 사용으로  DetailModal 컴포넌트 제거

--- src/components/myPage/Collection.jsx
+++ src/components/myPage/Collection.jsx
@@ -1,10 +1,8 @@
-import { useRef, Suspense, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Accordion } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import { useRecoilValue } from 'recoil';
 import { useCollectionQueries } from '../../hooks/queries';
 import { useSelectedItem } from '../../hooks';
-import { DetailModalWrapper, ModalSkeleton } from '../common';
 import { CollectionItem } from '.';
 import { sideNavState } from '../../recoil/atom';
 
@@ -12,9 +10,7 @@ const Collection = ({ collection, setIsItemSelected, setImgSrc, page }) => {
   const isNavOpened = useRecoilValue(sideNavState);
 
   const collectionQueries = useCollectionQueries(collection);
-  const [detailModalOpened, { open: openDetailModal, close: closeDetailModal }] = useDisclosure(false);
 
-  const [clicked, setClicked] = useState(null);
   const [selectedItem, setSelectedItem] = useState(null);
 
   const allQueriesSucceeded = collectionQueries.every(result => result.isSuccess);
@@ -51,18 +47,11 @@ const Collection = ({ collection, setIsItemSelected, setImgSrc, page }) => {
             <CollectionItem
               key={item.id}
               item={item}
-              setClicked={setClicked}
-              openDetailModal={openDetailModal}
               setSelectedItem={setSelectedItem}
               setIsItemSelected={setIsItemSelected}
             />
           ))}
       </Accordion>
-      {detailModalOpened && (
-        <Suspense fallback={<ModalSkeleton />}>
-          <DetailModalWrapper opened={detailModalOpened} close={closeDetailModal} id={clicked.id} type={clicked.type} />
-        </Suspense>
-      )}
     </>
   );
 };
