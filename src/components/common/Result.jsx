0c093be3b6f59f7f3aabed31dd2ffd74b698b08e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-05-18 02:14:37 +0900

    Feat: modals Manager 사용으로 Modal 컴포넌트 제거

--- src/components/common/Result.jsx
+++ src/components/common/Result.jsx
@@ -1,28 +1,37 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import parse from 'html-react-parser';
 import { Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 
 import styled from '@emotion/styled';
-import { DetailModalWrapper, ModalSkeleton } from '.';
+import { DetailModal, ModalSkeleton } from '.';
+
+import { modals } from '@mantine/modals';
 
 const Container = styled(Text)`
   cursor: pointer;
 `;
 
 const SearchResult = ({ id, title, name, reg, type }) => {
-  const [opened, { open, close }] = useDisclosure(false);
+  const DetailClick = () => {
+    modals.open({
+      centered: true,
+      withCloseButton: false,
+      size: 950,
+      padding: 0,
+      m: 0,
+      children: (
+        <Suspense fallback={<ModalSkeleton />}>
+          <DetailModal type={type} id={id} />
+        </Suspense>
+      ),
+    });
+  };
 
   return (
     <>
-      <Container key={id} mb="sm" fw={400} onClick={open}>
+      <Container key={id} mb="sm" fw={400} onClick={DetailClick}>
         {parse((title || name).replace(reg, match => `<b>${match}</b>`))}
       </Container>
-      {opened && (
-        <Suspense fallback={<ModalSkeleton />}>
-          <DetailModalWrapper opened={opened} close={close} id={id} type={type} />
-        </Suspense>
-      )}
     </>
   );
 };
