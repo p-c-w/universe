d3555da242f3c34d0fc641dae214779d9b3e2c48
5kdk <tylerk1121@gmail.com>
2023-05-19 02:27:11 +0900

    Remove: 불필요한 코드 제거

--- src/components/rootPage/Posters/Posters.jsx
+++ src/components/rootPage/Posters/Posters.jsx
@@ -9,13 +9,14 @@ import { ScrollObserver } from '../../common';
 
 const CardGrid = styled(SimpleGrid)`
   position: relative;
-  margin-bottom: var(--mantine-spacing-md);
+  min-width: 20rem;
+  margin: var(--mantine-spacing-md) auto;
 `;
 
 const observeOption = { rootMargin: '30%' };
 
 const Posters = ({ mediaType }) => {
-  const { data: content, hasNextPage, fetchNextPage } = useSortByPopularityInfinityQuery(mediaType);
+  const { content, hasNextPage, fetchNextPage } = useSortByPopularityInfinityQuery(mediaType);
   const smallScreen = useMediaQuery('(max-width: 96rem)');
 
   const getNextPage = useCallback(() => {
@@ -30,44 +31,41 @@ const Posters = ({ mediaType }) => {
     <>
       <CardGrid
         cols={5}
-        miw={320}
         maw={smallScreen ? '100%' : '80%'}
-        m="0 auto"
         verticalSpacing="sm"
         breakpoints={[
           { maxWidth: '80rem', cols: 4 },
           { maxWidth: '60rem', cols: 3 },
           { maxWidth: '40rem', cols: 2 },
         ]}>
-        {content &&
-          content.map(
-            ({
-              id,
-              title,
-              name,
-              original_title: originalTitle,
-              original_name: originalName,
-              poster_path: posterPath,
-              backdrop_path: backdropPath,
-              genre_ids: genreIds,
-              overview,
-              release_date: releaseDate,
-              first_air_date: firstAirDate,
-            }) => (
-              <Poster
-                key={id}
-                id={id}
-                title={movie ? title : name}
-                originalTitle={movie ? originalTitle : originalName}
-                posterPath={posterPath}
-                backdropPath={backdropPath}
-                genreIds={genreIds}
-                overview={overview}
-                date={movie ? releaseDate : firstAirDate}
-                mediaType={mediaType}
-              />
-            )
-          )}
+        {content.map(
+          ({
+            id,
+            title,
+            name,
+            original_title: originalTitle,
+            original_name: originalName,
+            poster_path: posterPath,
+            backdrop_path: backdropPath,
+            genre_ids: genreIds,
+            overview,
+            release_date: releaseDate,
+            first_air_date: firstAirDate,
+          }) => (
+            <Poster
+              key={id}
+              id={id}
+              title={movie ? title : name}
+              originalTitle={movie ? originalTitle : originalName}
+              posterPath={posterPath}
+              backdropPath={backdropPath}
+              genreIds={genreIds}
+              overview={overview}
+              date={movie ? releaseDate : firstAirDate}
+              mediaType={mediaType}
+            />
+          )
+        )}
       </CardGrid>
       <ScrollObserver loader={<PosterSkeleton />} hasNextPage={hasNextPage} observer={observerRef} />
     </>
