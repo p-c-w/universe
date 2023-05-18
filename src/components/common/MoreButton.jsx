d53cda47c8834be582c4e5d1c2e4035f750fdcf2
5kdk <tylerk1121@gmail.com>
2023-05-19 04:34:57 +0900

    Refactor: 핸들러 분리, 디자인 세부 사항 변경

--- src/components/common/MoreButton.jsx
+++ src/components/common/MoreButton.jsx
@@ -16,11 +16,11 @@ const MoreButton = ({ open, pos, right, left, top, bottom }) => (
     title="더보기"
     aria-label="more"
     onClick={open}>
-    <Text fz="xs" fw={200} color="gray.4">
-      {'더보기'}
+    <Text fz="sm" c="white">
+      더보기
     </Text>
-    <ThemeIcon variant="transparent" color="gray">
-      <IconLayersLinked size={16} color="gray" />
+    <ThemeIcon variant="varient" c="white">
+      <IconLayersLinked size={18} color="white" />
     </ThemeIcon>
   </Button>
 );
