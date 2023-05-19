dd4f4a6701c81768c1eec31050c506d9eddc28d8
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-19 13:02:39 +0900

    Refactor: 인수 사용에서 atom 사용으로 변경
    - usePagination 커스텀 훅 category 를 인수에서 atom으로 변경

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { categoryState } from '../recoil/atom';
 import { PAGE_LIMIT } from '../constants';
 
-const usePagination = (data, category) => {
+const usePagination = data => {
+  const category = useRecoilValue(categoryState);
+
   const [activePage, setActivePage] = useState(1);
   const offset = (activePage - 1) * PAGE_LIMIT;
   const total = Math.ceil(data.length / 5);
