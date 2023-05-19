3d44f41e91eb88d07a7b6cb6eacced2ff4bef2f3
5kdk <tylerk1121@gmail.com>
2023-05-19 16:44:34 +0900

    Design: 색상 prop명 변경 및 불필요한 fragment 제거

--- src/components/common/ModalSkeleton/ModalSkeleton.jsx
+++ src/components/common/ModalSkeleton/ModalSkeleton.jsx
@@ -5,7 +5,7 @@ const ModalSkeleton = () => (
     <Modal.Root opened={true} size={1000} m={0} p={0} centered zIndex="1001">
       <Modal.Overlay />
       <Modal.Content>
-        <Overlay c="#000000" opacity={0.5} zIndex="1" />
+        <Overlay c="black" opacity={0.5} zIndex="1" />
         <Modal.Body bg="none" m={0} p={0}>
           <Skeleton width={1000} height={562} />
         </Modal.Body>
