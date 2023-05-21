abd2bea5efba16402b953520865f47588de892f8
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-22 03:18:43 +0900

    Refactor: queryKey 상수화

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,5 +13,8 @@ export { userInfoQueryKey as USER_INFO_QUERY_KEY } from './userQueryKeys';
 export { userContentQueryKey as USER_CONTENT_QUERY_KEY } from './userQueryKeys';
 export { isAuthenticatedQueryKey as IS_AUTHENTICATED_QUERY_KEY } from './userQueryKeys';
 export { searchResultQueryKey as SEARCH_RESULT_QUERY_KEY } from './userQueryKeys';
+export { collectionQueryKey as COLLECTION_QUERY_KEY } from './userQueryKeys';
+export { contentDetailQueryKey as CONTENT_DETAIL_QUERY_KEY } from './userQueryKeys';
+export { providerQueryKey as PROVIDER_QUERY_KEY } from './userQueryKeys';
 
 // constants 정리합시다!
