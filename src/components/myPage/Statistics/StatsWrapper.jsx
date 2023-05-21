682a419e38aac902dd27ab0c8031ada3a21054a0
5kdk <tylerk1121@gmail.com>
2023-05-22 00:03:56 +0900

    Refactor: useMediaQuery 훅 제거
    Co-authored-by: Inae Choi <112180368+dev-CIA@users.noreply.github.com>
    Co-authored-by: Yunseul <117149045+yunseul-dev@users.noreply.github.com>

--- src/components/myPage/Statistics/StatsWrapper.jsx
+++ src/components/myPage/Statistics/StatsWrapper.jsx
@@ -1,5 +1,4 @@
 import { Paper, Title, Flex, Group } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
 import { IconDeviceAnalytics } from '@tabler/icons-react';
 import styled from '@emotion/styled';
 import { Suspense } from 'react';
@@ -8,22 +7,18 @@ const Icon = styled(IconDeviceAnalytics)`
   color: ${({ theme }) => (theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4])};
 `;
 
-const StatsWrapper = ({ stats }) => {
-  const smallScreen = useMediaQuery('(max-width: 48rem)');
-
-  return (
-    <Paper withBorder p={40} pt="sm" radius="md" h="100%">
-      <Flex direction="column" justify="center" h="100%">
-        <Group align="center" position="apart">
-          <Title order={3} align="left" fz={smallScreen ? 20 : 22}>
-            Universe 분석
-          </Title>
-          <Icon size="1.4rem" stroke={1.5} />
-        </Group>
-        <Suspense>{stats}</Suspense>
-      </Flex>
-    </Paper>
-  );
-};
+const StatsWrapper = ({ stats }) => (
+  <Paper withBorder p={40} pt="sm" radius="md" h="100%">
+    <Flex direction="column" justify="center" h="100%">
+      <Group align="center" position="apart">
+        <Title order={3} align="left" fz={20}>
+          Universe 분석
+        </Title>
+        <Icon size="1.4rem" stroke={1.5} />
+      </Group>
+      <Suspense>{stats}</Suspense>
+    </Flex>
+  </Paper>
+);
 
 export default StatsWrapper;
