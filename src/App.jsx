f83870e235ed073fafe7be91bdb46f663eb9d40f
5kdk <tylerk1121@gmail.com>
2023-05-22 02:01:56 +0900

    Feat: lazy loading 1차 구현완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -7,7 +8,6 @@ import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import useColorScheme from './hooks/useColorScheme';
 import AuthenticationGuard from './guard/AuthenticationGuard';
-import { Root, MyPage, SignIn, SignUp, EditProfile } from './pages';
 import { COLORS } from './constants';
 
 const queryClient = new QueryClient({
@@ -18,21 +18,31 @@ const queryClient = new QueryClient({
   },
 });
 
+const lazyLoadRoutes = componentName => {
+  const LazyElement = lazy(() => import(`./pages/${componentName}.jsx`));
+
+  return (
+    <Suspense fallback="Loading...">
+      <LazyElement />
+    </Suspense>
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: '/mypage',
-    element: <AuthenticationGuard redirectTo="/signin" element={<MyPage />} />,
+    element: <AuthenticationGuard redirectTo="/signin" element={lazyLoadRoutes('MyPage')} />,
   },
   {
     path: '/',
-    element: <Root />,
+    element: lazyLoadRoutes('Root'),
   },
   {
     path: '/editprofile',
-    element: <AuthenticationGuard redirectTo="/signin" element={<EditProfile />} />,
+    element: <AuthenticationGuard redirectTo="/signin" element={lazyLoadRoutes('EditProfile')} />,
   },
-  { path: '/signin', element: <SignIn /> },
-  { path: '/signup', element: <SignUp /> },
+  { path: '/signin', element: lazyLoadRoutes('SignIn') },
+  { path: '/signup', element: lazyLoadRoutes('SignUp') },
 ]);
 
 const App = () => {
@@ -44,7 +54,7 @@ const App = () => {
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
           <MantineProvider
             theme={{
-              fontFamily: 'Spoqa Han Sans Neo, sans-serif',
+              fontFamily: "'Spoqa Han Sans Neo', sans-serif",
               colorScheme,
               primaryColor: 'violet',
               colors: COLORS,
