b5c2297ac5dcfb33e906b2721c2819eea4d3e178
5kdk <tylerk1121@gmail.com>
2023-05-22 03:47:52 +0900

    Refactor: url 상수화

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,17 +1,13 @@
 export { default as PROVIDERS } from './providers';
 export { default as COLORS } from './colors';
 export { default as GENRES } from './genres';
-export { TMDB_IMG_URL, PLACEHOLDER_IMG_URL } from './urls';
-
+export { TMDB_IMG_URL, PLACEHOLDER_IMG_URL, PCW_REPO_URL, TMDB_URL } from './urls';
 export { default as COLLECTION_BUTTON } from './collectionButtons';
 export { default as PAGE_LIMIT } from './pageLimit';
 export { default as SIDE_NAV_DURATION } from './sideNavDuration';
 export { default as BUTTON_START_Y } from './buttonStartY';
 export { default as SCROLL_DELAY } from './scrollDelay';
-
 export { userInfoQueryKey as USER_INFO_QUERY_KEY } from './userQueryKeys';
 export { userContentQueryKey as USER_CONTENT_QUERY_KEY } from './userQueryKeys';
 export { isAuthenticatedQueryKey as IS_AUTHENTICATED_QUERY_KEY } from './userQueryKeys';
 export { searchResultQueryKey as SEARCH_RESULT_QUERY_KEY } from './userQueryKeys';
-
-// constants 정리합시다!
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,4 @@
-export const TMDB_IMG_URL = `https://image.tmdb.org/t/p/`;
-export const PLACEHOLDER_IMG_URL = `https://placehold.co/`;
+export const TMDB_URL = 'https://www.themoviedb.org/';
+export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/';
+export const PLACEHOLDER_IMG_URL = 'https://placehold.co/';
+export const PCW_REPO_URL = 'https://github.com/p-c-w';
