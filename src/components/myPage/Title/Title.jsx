91c6cf088241029922d5243c9632b3df46a7e28e
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-22 13:22:44 +0900

    Refactor: Mypage Title loader 수정

--- src/components/myPage/Title/Title.jsx
+++ src/components/myPage/Title/Title.jsx
@@ -1,10 +1,10 @@
 import React, { Suspense } from 'react';
-import { Container } from '@mantine/core';
+import { Container, Box } from '@mantine/core';
 import { TitleContent } from '.';
 
 const Title = () => (
   <Container m={0} mb={16} p={0} spacing={5} miw={350} display="flex" gap={3.2} align="start">
-    <Suspense>
+    <Suspense fallback={<Box miw={350} w={768} h={52} />}>
       <TitleContent />
     </Suspense>
   </Container>
