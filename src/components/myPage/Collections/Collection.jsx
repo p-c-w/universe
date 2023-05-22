dbde3326c8e711dd34242ed98bed2ba84382840b
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-22 12:32:26 +0900

    Comment: 주석 제거

--- src/components/myPage/Collections/Collection.jsx
+++ src/components/myPage/Collections/Collection.jsx
@@ -35,19 +35,13 @@ const Collection = ({ setImgSrc }) => {
     setImgSrc(imgUrl.current);
   };
 
-  // 새로운 파생상태나 전역상태 말고 key로도 해결할 수 있을거 같아요.
   return (
     <>
       {collectionList.length === 0 ? (
         <EmptyMessage category={selectedCategory} />
       ) : (
         <>
-          <Accordion
-            // key={category + activePage}
-            variant="separated"
-            w="100%"
-            onChange={selectItem}
-            value={selectedItem}>
+          <Accordion variant="separated" w="100%" onChange={selectItem} value={selectedItem}>
             {collectionList?.map(item => (
               <Item key={item.id} item={item} />
             ))}
