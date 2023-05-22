1fda3d3201b4cb3e103e6ac4cd16845f42c1af7f
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-22 15:42:03 +0900

    Feat: 추천구독정보 스켈레톤 구현

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,3 +1,4 @@
 export { default as PosterSkeleton } from './PosterSkeleton';
 export { default as CarouselSkeleton } from './CarouselSkeleton';
 export { default as CollectionSkeleton } from './CollectionSkeleton';
+export { default as SuggestedSubscriptionSkeleton } from './SuggestedSubscriptionSkeleton';
