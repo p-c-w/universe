4932118a3515cde90a69ba547f9ff81d9633f39d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-05-22 12:20:08 +0900

    Refactor: side nav와 screen size에 따른 MyInfo 반응형 디자인

--- src/components/myPage/MyInfo/MyInfo.jsx
+++ src/components/myPage/MyInfo/MyInfo.jsx
@@ -1,21 +1,34 @@
 import { Suspense } from 'react';
+import { useRecoilValue } from 'recoil';
 import { Box, SimpleGrid, Skeleton } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import { sideNavState } from '../../../recoil/atom';
 import { Statistics, CurrentSubscription, SuggestedSubscription } from '..';
 
-const MyInfo = () => (
-  <SimpleGrid cols={2} mt={32} spacing="xl" breakpoints={[{ maxWidth: '60rem', cols: 1 }]}>
-    <Box>
+const MyInfo = () => {
+  const largeScreen = useMediaQuery('(max-width: 75rem)');
+  const middleScreen = useMediaQuery('(max-width: 60rem)');
+  const isOpened = useRecoilValue(sideNavState);
+
+  return (
+    <SimpleGrid
+      cols={middleScreen || (largeScreen && isOpened) ? 1 : 2}
+      mt={32}
+      spacing="xl"
+      breakpoints={[{ maxWidth: '60rem', cols: 1 }]}>
+      <Box>
+        <Suspense fallback={<Skeleton w={500} h="100%" />}>
+          <SuggestedSubscription />
+        </Suspense>
+        <Suspense fallback={<Skeleton w={500} h="100%" />}>
+          <CurrentSubscription />
+        </Suspense>
+      </Box>
       <Suspense fallback={<Skeleton w={500} h="100%" />}>
-        <SuggestedSubscription />
+        <Statistics />
       </Suspense>
-      <Suspense fallback={<Skeleton w={500} h="100%" />}>
-        <CurrentSubscription />
-      </Suspense>
-    </Box>
-    <Suspense fallback={<Skeleton w={500} h="100%" />}>
-      <Statistics />
-    </Suspense>
-  </SimpleGrid>
-);
+    </SimpleGrid>
+  );
+};
 
 export default MyInfo;
