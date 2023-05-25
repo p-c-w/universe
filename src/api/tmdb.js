d0c2cd37287c4311224878eb5f3e8199e0d46b14
5kdk <tylerk1121@gmail.com>
2023-05-25 15:07:44 +0900

    Coment: 주석 삭제

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,20 +1,13 @@
 import axios from 'axios';
+import { TMDB_API_URL } from '../constants';
 
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const API_URL = import.meta.env.VITE_TMDB_API_URL;
+const { VITE_TMDB_API_KEY } = import.meta.env;
 const language = 'ko-KR';
 
-/**
- * 미디어 콘텐츠 인기순 정렬 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {number} page
- * @param {string} providerId
- * @returns data
- */
 export const fetchSortByPopularity = async (mediaType, page, providerId = '8|119|337|356|97|350') => {
-  const res = await axios.get(`${API_URL}discover/${mediaType}`, {
+  const res = await axios.get(`${TMDB_API_URL}discover/${mediaType}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
       sort_by: 'popularity.desc',
       page,
@@ -26,16 +19,10 @@ export const fetchSortByPopularity = async (mediaType, page, providerId = '8|119
   return res.data;
 };
 
-/**
-/**
- * 미디어 콘텐츠 릴리즈 날짜순 정렬 데이터
- * @param {'movie' | 'tv'} mediaType
- * @returns data
- */
 export const fetchSortByReleaseDate = async (mediaType, providerId) => {
-  const res = await axios.get(`${API_URL}discover/${mediaType}`, {
+  const res = await axios.get(`${TMDB_API_URL}discover/${mediaType}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
       sort_by: 'release_date.desc',
       watch_region: 'KR',
@@ -46,17 +33,10 @@ export const fetchSortByReleaseDate = async (mediaType, providerId) => {
   return res.data;
 };
 
-/**
- *  미디어 콘텐츠 장르 인기순 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {string | number} genreId
- * @param {string | number} providerId
- * @returns
- */
 export const fetchWithGenre = async (mediaType, genreId, providerId) => {
-  const res = await axios.get(`${API_URL}discover/${mediaType}`, {
+  const res = await axios.get(`${TMDB_API_URL}discover/${mediaType}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
       sort_by: 'popularity.desc',
       watch_region: 'KR',
@@ -67,32 +47,20 @@ export const fetchWithGenre = async (mediaType, genreId, providerId) => {
   return res.data;
 };
 
-/**
- * 미디어 콘텐츠 디테일 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {string | number} id
- * @returns
- */
 export const fetchMediaContentDetails = async (mediaType, contentId) => {
-  const res = await axios.get(`${API_URL + mediaType}/${contentId}`, {
+  const res = await axios.get(`${TMDB_API_URL + mediaType}/${contentId}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
     },
   });
   return res.data;
 };
 
-/**
- * 미디어 콘텐츠 검색 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {string} query
- * @returns data
- */
 export const fetchSearchResult = async (mediaType, query) => {
-  const res = await axios.get(`${API_URL}search/${mediaType}`, {
+  const res = await axios.get(`${TMDB_API_URL}search/${mediaType}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
       query,
     },
@@ -100,37 +68,25 @@ export const fetchSearchResult = async (mediaType, query) => {
   return res.data;
 };
 
-/**
- * 미디어 콘텐츠 OTT서비스 provider 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {string | number} id
- * @returns
- */
 export const fetchProvider = async (mediaType, id) => {
   const res = await axios.get(`${API_URL + mediaType}/${id}/watch/providers`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
     },
   });
   return res.data;
 };
 
-/**
- * 미디어 콘텐츠 OTT서비스 provider와 detail 데이터
- * @param {'movie' | 'tv'} mediaType
- * @param {string | number} id
- * @returns
- */
 export const fetchProviderAndDetail = async (mediaType, id) => {
   const providerRes = await axios.get(`${API_URL + mediaType}/${id}/watch/providers`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
     },
   });
 
   const DetailRes = await axios.get(`${API_URL + mediaType}/${id}`, {
     params: {
-      api_key: API_KEY,
+      api_key: VITE_TMDB_API_KEY,
       language,
     },
   });
