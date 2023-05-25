9e817dcc0f7171bdad6b23dc34993fa037480925
5kdk <tylerk1121@gmail.com>
2023-05-25 19:36:36 +0900

    Refactor: 배포를 위한 서버 url 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,39 +2,46 @@ import axios from 'axios';
 
 const url = `/api/auth`;
 
+const { VITE_CORS_SERVER_URL = '' } = import.meta.env;
+
+const checkVerify = async () => {
+  const res = await axios(`${VITE_CORS_SERVER_URL}${url}/verify`);
+  return res.data;
+};
+
 const signIn = async data => {
-  const { data: user } = await axios.post(`${url}/signin`, data);
+  const { data: user } = await axios.post(`${VITE_CORS_SERVER_URL}${url}/signin`, data);
   return user;
 };
 
 const signUp = async data => {
   const {
     data: { email, name },
-  } = await axios.post(`${url}/signup`, data);
+  } = await axios.post(`${VITE_CORS_SERVER_URL}${url}/signup`, data);
 
   return { email, name };
 };
 
 const signOut = async () => {
-  const { data } = await axios.get(`${url}/signout`);
+  const { data } = await axios.get(`${VITE_CORS_SERVER_URL}${url}/signout`);
 
   return data.isLogin;
 };
 
 const submitSubscribedOtt = async (email, subscribedOtt) =>
-  axios.patch(`${url}/${email}`, { subscribe_list: subscribedOtt });
+  axios.patch(`${VITE_CORS_SERVER_URL + url}/${email}`, { subscribe_list: subscribedOtt });
 
 const deleteUser = async user => {
   const {
     data: { isLogin, message },
-  } = await axios.delete(`${url}/withdrawal/${user}`);
+  } = await axios.delete(`${VITE_CORS_SERVER_URL}${url}/withdrawal/${user}`);
 
   return { isLogin, message };
 };
 
 const changePassword = async (email, data) => {
-  const { data: message } = await axios.patch(`${url}/changepw`, { email, ...data });
+  const { data: message } = await axios.patch(`${VITE_CORS_SERVER_URL}${url}/changepw`, { email, ...data });
   return message;
 };
 
-export { signIn, signUp, signOut, submitSubscribedOtt, deleteUser, changePassword };
+export { checkVerify, signIn, signUp, signOut, submitSubscribedOtt, deleteUser, changePassword };
