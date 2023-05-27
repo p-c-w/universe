58e2027ae0e888eabef44fa449914ea3bb48419c
5kdk <tylerk1121@gmail.com>
2023-05-27 22:36:17 +0900

    Fix: 스켈레톤 반응형 스타일 누락 추가

--- src/loaders/PosterSkeleton.jsx
+++ src/loaders/PosterSkeleton.jsx
@@ -1,31 +1,41 @@
 import { SimpleGrid, Skeleton } from '@mantine/core';
 import styled from '@emotion/styled';
 import { useMediaQuery } from '@mantine/hooks';
+import { useRecoilValue } from 'recoil';
+import { sideNavState } from '../recoil/atom';
 
 const CardGrid = styled(SimpleGrid)`
   position: relative;
-  margin: 0 auto;
+  min-width: 20rem;
+  margin: var(--mantine-spacing-md) auto;
 `;
 
 const StyledSkeleton = styled(Skeleton)`
   aspect-ratio: 3/4;
+  min-height: 6.25rem;
 `;
 
 const PosterSkeleton = () => {
-  const smallScreen = useMediaQuery('(max-width: 100rem)');
+  const isSideNavOpened = useRecoilValue(sideNavState);
+  const smallScreen = useMediaQuery(`(max-width: ${isSideNavOpened ? '100rem' : '90rem'})`);
 
   return (
     <CardGrid
       cols={5}
-      miw={320}
       maw={smallScreen ? '100%' : '80%'}
-      m="0 auto"
       verticalSpacing="sm"
-      breakpoints={[
-        { maxWidth: '80rem', cols: 4 },
-        { maxWidth: '60rem', cols: 3 },
-        { maxWidth: '40rem', cols: 2 },
-      ]}>
+      breakpoints={
+        isSideNavOpened
+          ? [
+              { maxWidth: '110rem', cols: 4 },
+              { maxWidth: '80rem', cols: 3 },
+              { maxWidth: '60rem', cols: 2 },
+            ]
+          : [
+              { maxWidth: '80rem', cols: 4 },
+              { maxWidth: '60rem', cols: 3 },
+            ]
+      }>
       {Array.from({ length: 20 }, (_, i) => (
         <StyledSkeleton key={i} radius="md" />
       ))}
