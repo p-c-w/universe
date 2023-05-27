8c44d148678aa4a141de8950e5718397beb3e9ce
5kdk <tylerk1121@gmail.com>
2023-05-27 22:30:07 +0900

    Fix: 백엔드 url 누락 추가

--- src/api/user.js
+++ src/api/user.js
@@ -2,20 +2,25 @@ import axios from 'axios';
 
 const url = `/api/users`;
 
+const { VITE_CORS_SERVER_URL = '' } = import.meta.env;
+
 const fetchUser = async email => {
-  const { data } = await axios.get(`${url}/${email}`);
+  const { data } = await axios.get(`${VITE_CORS_SERVER_URL}${url}/${email}`);
   return data;
 };
 
-const updateUserName = async ({ email, name }) => axios.patch(`${url}/${email}`, { name });
+const updateUserName = async ({ email, name }) => axios.patch(`${VITE_CORS_SERVER_URL}${url}/${email}`, { name });
 
-const updateUserContent = async ({ email, list, value }) => axios.patch(`${url}/${email}/${list}`, value);
+const updateUserContent = async ({ email, list, value }) =>
+  axios.patch(`${VITE_CORS_SERVER_URL}${url}/${email}/${list}`, value);
 
-const updateSubscribeList = async ({ email, newList }) => axios.patch(`${url}/${email}`, { subscribe_list: newList });
+const updateSubscribeList = async ({ email, newList }) =>
+  axios.patch(`${VITE_CORS_SERVER_URL}${url}/${email}`, { subscribe_list: newList });
 
 const useUpdateModifiedAt = async ({ email, list, id, value }) =>
-  axios.patch(`${url}/${email}/${list}/${id}`, { modified_at: value });
+  axios.patch(`${VITE_CORS_SERVER_URL}${url}/${email}/${list}/${id}`, { modified_at: value });
 
-const deleteUserContent = async ({ email, list, id }) => axios.delete(`${url}/${email}/${list}/${id}`);
+const deleteUserContent = async ({ email, list, id }) =>
+  axios.delete(`${VITE_CORS_SERVER_URL}${url}/${email}/${list}/${id}`);
 
 export { fetchUser, updateUserName, updateUserContent, updateSubscribeList, useUpdateModifiedAt, deleteUserContent };
